08bedeaa9adc57e3d921e4c2d5ca23824ab1f2e6
Paul van Schayck <polleke@gmail.com>
2016-06-30 15:53:52 +0200

    Switch to output JSON

--- list-projects.r
+++ list-projects.r
@@ -3,16 +3,15 @@
 # irule -F list-projects.r
 
 listProjects {
-    # writeLine("stdout", '[');
+    *json_str = '[]';
+    *size = 0;
 
     foreach ( *Row in SELECT COLL_NAME WHERE COLL_ACCESS_NAME = 'modify object' and COLL_PARENT_NAME = '/nlmumc/projects' ) {
         uuChopPath(*Row.COLL_NAME, *collection, *project);
-
-        # writeLine("stdout", "   \"*project\",");
-        writeLine("stdout", "*project");
+        msi_json_arrayops(*json_str, *project, "add", *size)
     }
 
-    # writeLine("stdout", ']');
+    writeLine("stdout", *json_str);
 }
 
 

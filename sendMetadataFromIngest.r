c70d0bbe0ce4d84567e9bdc386a61fcc829c14c6
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-09-09 15:47:40 +0200

    fixed bug where mirthURL was not in function definition

--- sendMetadataFromIngest.r
+++ sendMetadataFromIngest.r
@@ -6,7 +6,7 @@ irule_dummy() {
     IRULE_sendMetadataFromIngest(*token)
 }
 
-IRULE_sendMetadataFromIngest(*token) {
+IRULE_sendMetadataFromIngest(*token,*mirthURL) {
     # TODO: Parametrize this URL somehow
     #msi_http_send_file("http://fhml-srv024.unimaas.nl:6669/?token=*token", "/nlmumc/ingest/zones/*token/metadata.xml")
 	msi_http_send_file("*mirthURL/?token=*token", "/nlmumc/ingest/zones/*token/metadata.xml")

cd44d05cc1577884bcdb7814a82aac343fed21da
Paul van Schayck <polleke@gmail.com>
2016-10-13 14:59:11 +0200

    Add title to project information. Fixes RITDEV-101.

--- listContributingProjects.r
+++ listContributingProjects.r
@@ -8,7 +8,22 @@ listContributingProjects {
 
     foreach ( *Row in SELECT COLL_NAME WHERE COLL_ACCESS_NAME = 'modify object' and COLL_PARENT_NAME = '/nlmumc/projects' ) {
         uuChopPath(*Row.COLL_NAME, *collection, *project);
-        msi_json_arrayops(*json_str, *project, "add", *size)
+
+        foreach (*av in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME == "/nlmumc/projects/*project") {
+            if ( *av.META_COLL_ATTR_NAME == "title" ) {
+                *title = *av.META_COLL_ATTR_VALUE;
+            }
+        }
+
+        msiString2KeyValPair("", *kvp);
+        *o = "";
+
+        msiAddKeyVal(*kvp, 'project', *project);
+        msiAddKeyVal(*kvp, 'title', *title);
+
+        msi_json_objops(*o, *kvp, "set");
+
+        msi_json_arrayops(*json_str, *o, "add", *size)
     }
 
     writeLine("stdout", *json_str);

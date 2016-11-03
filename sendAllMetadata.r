7025cb08b7b8339a1f4dbe8396c8a850dc13a8ca
Paul van Schayck <polleke@gmail.com>
2016-11-03 13:20:58 +0100

    Fix sendAllMetadata to also send the correct mirth URL

--- sendAllMetadata.r
+++ sendAllMetadata.r
@@ -4,12 +4,14 @@
 
 sendAllMetadata() {
 
+    msi_getenv("MIRTH_METADATA_CHANNEL", *mirthMetaDataUrl);
+
     foreach ( *Row in select COLL_NAME, COLL_PARENT_NAME where COLL_NAME like '/nlmumc/projects/%/%' ) {
         uuChopPath(*Row.COLL_NAME, *head, *collection);
         uuChopPath(*Row.COLL_PARENT_NAME, *head, *project);
 
         writeLine("stdout", "Sending " ++ *project ++ " " ++ *collection);
-        sendMetadata(*project, *collection);
+        sendMetadata(*mirthMetaDataUrl, *project, *collection);
     }
 }
 

5419b7255e37a9920cbd6938f3d63373e78eff64
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-11-09 12:05:50 +0100

    Fixed bug where 'sendAllMetadata.r' would crash for collections where no 'metadata.xml' file is present

--- sendAllMetadata.r
+++ sendAllMetadata.r
@@ -6,7 +6,7 @@ sendAllMetadata() {
 
     msi_getenv("MIRTH_METADATA_CHANNEL", *mirthMetaDataUrl);
 
-    foreach ( *Row in select COLL_NAME, COLL_PARENT_NAME where COLL_NAME like '/nlmumc/projects/%/%' ) {
+    foreach ( *Row in select COLL_NAME, COLL_PARENT_NAME, DATA_NAME where COLL_NAME like '/nlmumc/projects/%/%' and DATA_NAME = "metadata.xml") {
         uuChopPath(*Row.COLL_NAME, *head, *collection);
         uuChopPath(*Row.COLL_PARENT_NAME, *head, *project);
 

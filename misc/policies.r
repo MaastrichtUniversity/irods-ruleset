c60effeec79f6788b67c7708b6a43a7c128c1293
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2017-02-24 09:09:09 +0100

    Policies to prevent folder or file creation at P-level. For RITDEV-162

--- misc/policies.r
+++ misc/policies.r
@@ -1,27 +1,56 @@
 # Policies
 
 acSetRescSchemeForCreate {
-    if($objPath like "/nlmumc/projects/*/*") {
-        *resource = "";
+    ### Policy to set proper storage resource & prevent file creation directly in P-folder ###
+    if($objPath like regex "/nlmumc/projects/P........./.*") {
+        if($objPath like regex "/nlmumc/projects/P........./C........./.*") {
+            # This is a proper location to store project files
+            *resource = "";
 
-        uuChop($objPath, *head, *tail, "/nlmumc/projects/", true);
-        uuChop(*tail, *project, *tail, "/", true);
+            uuChop($objPath, *head, *tail, "/nlmumc/projects/", true);
+            uuChop(*tail, *project, *tail, "/", true);
 
-        foreach (*av in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME == "/nlmumc/projects/*project") {
-            if ( *av.META_COLL_ATTR_NAME == "resource" ) {
-                *resource = *av.META_COLL_ATTR_VALUE;
+            foreach (*av in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME == "/nlmumc/projects/*project") {
+                if ( *av.META_COLL_ATTR_NAME == "resource" ) {
+                    *resource = *av.META_COLL_ATTR_VALUE;
+                }
             }
+
+            if ( *resource == "") {
+                failmsg(-1, "resource is empty!");
+            }
+
+            msiWriteRodsLog("DEBUG: Setting resource for project *project to resource *resource", *status);
+            msiSetDefaultResc(*resource, "forced");
+        } else {
+            # Somebody tries to create a file outside of a projectcollection
+            uuChop($objPath, *head, *tail, "/nlmumc/projects/", true);
+            uuChop(*tail, *project, *tail, "/", true);
+
+            msiWriteRodsLog("DEBUG: Not allowed to create files in projecfolder '*project' for objPath '$objPath'", *status);
+            cut;
+            msiOprDisallowed;
         }
-
-        if ( *resource == "") {
-            failmsg(-1, "resource is empty!");
-        }
-
-        msiWriteRodsLog("Setting resource for project *project to resource *resource", *status);
-
-        msiSetDefaultResc(*resource, "forced");
     } else {
+        # We are not in a projectfolder at all
         msiSetDefaultResc("rootResc","null");
+    }
+}
+
+acPreprocForCollCreate {
+    ### Policy to regulate folder creation within projects ###
+    if($collName like regex "/nlmumc/projects/P........./.*") {
+        if($collName like regex "/nlmumc/projects/P........./C........." || $collName like regex "/nlmumc/projects/P........./C........./.*") {
+            # Creating a C-folder or subfolder of projectcollection
+            #msiWriteRodsLog("DEBUG: Creating folder '$collName'", *status);
+        } else {
+            # Creating a non-C folder at project level
+            msiWriteRodsLog("DEBUG: Folder '$collName' not compliant with naming convention", *status);
+            cut;
+            msiOprDisallowed;
+        }
+    } else {
+        #msiWriteRodsLog("DEBUG: No create-policy active outside of projects. Creating folder '$collName'", *status);
     }
 }
 

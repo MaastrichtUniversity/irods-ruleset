df78ff4daa5300d79a81da4c67b2f70f2e3cfe1d
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2017-07-18 15:53:07 +0200

    Add project as a property in getprojectdetails.r

--- projects/detailsProject.r
+++ projects/detailsProject.r
@@ -17,8 +17,8 @@ IRULE_detailsProject(*project, *result) {
 
     getCollectionAVU("/nlmumc/projects/*project","resource",*resource,"","true");
     getCollectionAVU("/nlmumc/projects/*project","title",*title,"","true");
-
-    *details = '{ "resource": "*resource", "viewers": *viewers,"contributors": *contributors, "managers": *managers}';
+    
+    *details = '{"project":"*project", "resource": "*resource", "viewers": *viewers,"contributors": *contributors, "managers": *managers}';
 
     # Title needs proper escaping before adding to JSON. That's why we pass it through msi_json_objops
     msiString2KeyValPair("", *titleKvp);

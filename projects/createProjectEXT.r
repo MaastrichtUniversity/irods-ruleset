16d7114d6894fc52dab4191d84a01377270854f7
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2018-01-10 15:24:39 +0100

    created extended version of the createProject rule for RITDEV-363

---
+++ projects/createProjectEXT.r
@@ -0,0 +1,57 @@
+# Call with
+#
+# irule -F createProjectEXT.r "*authorizationPeriodEndDate='1-1-2018'" "*dataRetentionPeriodEndDate='1-1-2018'" "*ingestResource='iresResource'" "*resource='replRescUM01'" "*storageQuotaGb='10'" "*title='Testing'"
+
+irule_dummy() {
+    IRULE_createProjectEXT(*result,*authorizationPeriodEndDate,*dataRetentionPeriodEndDate,*ingestResource,*resource,*storageQuotaGb,*title);
+    writeLine("stdout", *result);
+}
+
+
+# Creates projects in the form P000000001
+IRULE_createProjectEXT(*project,*authorizationPeriodEndDate,*dataRetentionPeriodEndDate,*ingestResource,*resource,*storageQuotaGb,*title) {
+
+    *max = 0;
+
+    # Find out the current max project number
+    foreach ( *Row in SELECT COLL_NAME WHERE COLL_PARENT_NAME = '/nlmumc/projects' ) {
+        uuChopPath(*Row.COLL_NAME, *path, *c);
+
+        *i = int(substr(*c, 1, 10));
+
+        if ( *i > *max ) {
+            *max = *i;
+        }
+    }
+
+    *project = str(*max + 1);
+
+    # Prepend padding zeros to the name
+    while ( strlen(*project) < 9 ) {
+        *project = "0" ++ *project;
+    }
+
+    *project = "P" ++ *project;
+
+    *dstColl = "/nlmumc/projects/*project";
+
+    msiCollCreate(*dstColl, 0, *status);
+
+    # TODO: Determine whether setting defaults here is a good place
+    msiAddKeyVal(*metaKV, "authorizationPeriodEndDate", *authorizationPeriodEndDate);
+    msiAddKeyVal(*metaKV, "dataRetentionPeriodEndDate", *dataRetentionPeriodEndDate);
+    msiAddKeyVal(*metaKV, "ingestResource", *ingestResource);
+    msiAddKeyVal(*metaKV, "resource", *resource);
+    msiAddKeyVal(*metaKV, "title", *title);
+    msiSetKeyValuePairsToObj(*metaKV, *dstColl, "-C");
+
+
+    # Set recursive permissions
+    msiSetACL("default", "read", "service-dwh", *dstColl);
+    msiSetACL("default", "write", "service-pid", *dstColl);
+    msiSetACL("recursive", "inherit", "", *dstColl);
+    
+}
+
+INPUT *authorizationPeriodEndDate="", *dataRetentionPeriodEndDate="", *ingestResource="", *resource="", *storageQuotaGb="", *title=""
+OUTPUT ruleExecOut

d4964a493e6dfead61f693ea55173a502c82a844
Paul van Schayck <polleke@gmail.com>
2018-01-11 12:05:36 +0100

    getDestinationResources and getIngestResources

---
+++ misc/getIngestResources.r
@@ -0,0 +1,25 @@
+# Call with
+#
+# irule -F getIngestResources.r
+
+irule_dummy() {
+    IRULE_getIngestResources(*result);
+    writeLine("stdout", *result);
+}
+
+IRULE_getIngestResources(*result) {
+    *resources = '[]';
+    *resourcesSize = 0;
+
+    foreach ( *Row in select RESC_NAME, RESC_COMMENT where RESC_VAULT_PATH = '/var/lib/irods/vault') {
+        *name = *Row.RESC_NAME
+        *comment = *Row.RESC_COMMENT
+        *r = ' { "name": "*name", "comment": "*comment" } '
+        msi_json_arrayops(*resources, *r, "add", *resourcesSize);
+    }
+
+    *result = *resources;
+}
+
+INPUT null
+OUTPUT ruleExecOut

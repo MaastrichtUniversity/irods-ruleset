cb1a9179a04a75c31c2b567e9c3926a7a9f242f9
Paul van Schayck <polleke@gmail.com>
2018-01-11 12:05:36 +0100

    getDestinationResources and getIngestResources

---
+++ misc/getDestinationResources.r
@@ -0,0 +1,25 @@
+# Call with
+#
+# irule -F getDestinationResources.r
+
+irule_dummy() {
+    IRULE_getDestinationResources(*result);
+    writeLine("stdout", *result);
+}
+
+IRULE_getDestinationResources(*result) {
+    *resources = '[]';
+    *resourcesSize = 0;
+
+    foreach ( *Row in select RESC_NAME, RESC_COMMENT where RESC_LOC = 'EMPTY_RESC_HOST') {
+        *name = *Row.RESC_NAME
+        *comment = *Row.RESC_COMMENT
+        *r = ' { "name": "*name", "comment": "*comment" } '
+        msi_json_arrayops(*resources, *r, "add", *resourcesSize);
+    }
+
+    *result = *resources;
+}
+
+INPUT null
+OUTPUT ruleExecOut

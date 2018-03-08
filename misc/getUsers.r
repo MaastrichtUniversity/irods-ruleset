2f06656ab91a1b63b6af36d9fad554dd96c6ada4
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2018-03-08 16:21:42 +0100

    getUsers rule now has a parameter showServiceAccounts to return or not return service accounts. Default is to return service accounts

--- misc/getUsers.r
+++ misc/getUsers.r
@@ -1,22 +1,29 @@
 # Call with
 #
-# irule -F getUsers.r
+# irule -F getUsers.r "*showServiceAccounts='true'"
 
 irule_dummy() {
-    IRULE_getUsers(*result);
+    IRULE_getUsers(*showServiceAccounts , *result);
     writeLine("stdout", *result);
 }
 
-IRULE_getUsers(*result) {
+IRULE_getUsers(*showServiceAccounts,*result) {
     *users = '[]';
     *usersSize = 0;
 
     foreach ( *Row in select USER_NAME where USER_TYPE = 'rodsuser') {
-        msi_json_arrayops(*users, *Row.USER_NAME, "add", *usersSize);
+         if ( *showServiceAccounts == "false" ) {
+               if ( substr(*Row.USER_NAME, 0, 8) != "service-" ) {
+                      msi_json_arrayops(*users, *Row.USER_NAME, "add", *usersSize);
+               }
+         }
+         else{
+            msi_json_arrayops(*users, *Row.USER_NAME, "add", *usersSize);
+         }
     }
 
     *result = *users;
 }
 
-INPUT null
+INPUT *showServiceAccounts='true'
 OUTPUT ruleExecOut

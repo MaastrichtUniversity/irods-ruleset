8906e9be70781831927995996f17d357778a47c8
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2018-04-26 13:45:12 +0200

    Change of openProjectCollection.r to use admin option and user and right are now parameters for RITDEV-425

--- projects/openProjectCollection.r
+++ projects/openProjectCollection.r
@@ -1,28 +1,20 @@
 # Call with
 #
-# irule -F openProjectCollection.r "*project='P000000001'" "*projectCollection='C000000001'"
+# irule -F openProjectCollection.r "*project='P000000001'" "*projectCollection='C000000001'" "*user='rods'" "*rights='own'"
 #
-# This rule reopens a projectCollection in order to add, modify or delete data by an admin.
-# Since the rodsadmin user has readonly permissions on collection-level at the start of this rule
-# (= result of closeProjectCollection.r), we have to reopn the collection in a two-step fashion (see comments inline).
+# This rule reopens a projectCollection in order to add, modify or delete data by an user. It uses user=rods and right=own as default
+# for backwards compatibility
 
 irule_dummy() {
-    IRULE_openProjectCollection(*project, *projectCollection);
+    IRULE_openProjectCollection(*project, *projectCollection, *user, *rights);
 }
 
-IRULE_openProjectCollection(*project, *projectCollection) {
+IRULE_openProjectCollection(*project, *projectCollection, *user, *rights) {
 
     # Recursively assign ownership rights for rodsadmin to all collections within project
-    msiSetACL("recursive", "own", "rods", "/nlmumc/projects/*project");
+    msiSetACL("recursive", "admin:*rights", "*user", "/nlmumc/projects/*project/*projectCollection");
 
-    # Reset 'read' permission on all collections, except for the one that we want to open
-    foreach ( *Row in select COLL_NAME where COLL_PARENT_NAME = "/nlmumc/projects/*project" ) {
-        #writeLine("stdout", *Row.COLL_NAME);
-        if (*Row.COLL_NAME != "/nlmumc/projects/*project/*projectCollection") {
-            msiSetACL("recursive", "read", "rods", *Row.COLL_NAME);
-        }
-    }
 }
 
-INPUT *project='', *projectCollection=''
+INPUT *project='', *projectCollection='', *user='rods' , *rights='own'
 OUTPUT ruleExecOut

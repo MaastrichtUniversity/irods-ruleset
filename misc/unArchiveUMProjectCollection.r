7c5c15dc4f89863dd07e98335951e7856b81ff6f
Paul van Schayck <polleke@gmail.com>
2018-05-17 14:13:36 +0200

    Use new openProjectCollection, also in unArchive. DH-441.

--- misc/unArchiveUMProjectCollection.r
+++ misc/unArchiveUMProjectCollection.r
@@ -9,7 +9,7 @@ irule_dummy() {
 
 IRULE_unArchiveUMProjectCollection(*project, *projectCollection) {
     # Open project collection
-    openProjectCollection(*project, *projectCollection)
+    openProjectCollection(*project, *projectCollection, 'rods', 'own')
 
     # Obtain the project resource from the project
     getCollectionAVU("/nlmumc/projects/*project","resource",*sourceResource,"","true");

695dd1dedbe5e1762840e0b5235befeaedeb1823
Paul van Schayck <polleke@gmail.com>
2018-05-29 17:05:29 +0200

    Fix typo. DH-441

--- misc/untarProjectCollection.r
+++ misc/untarProjectCollection.r
@@ -49,10 +49,10 @@ IRULE_untarProjectCollection(*Tar, *Resc){
     # Opens our checksum file.
     msiDataObjOpen(*Coll++"/"++*CheckSums,*CKsums);
 
-    # (2^32)/2 - 1 = 2147483648
+    # (2^32)/2 - 1 = 2147483647
     # Could not figure out a way to find the actual size of the file. Instead trying to read the maximum size that's
     # possible. Not pretty, but workable
-    msiDataObjRead(*CKsums, 2147483648, *file_BUF);
+    msiDataObjRead(*CKsums, 2147483647, *file_BUF);
 
     # To prevent the searching of similarily named collections (such as ~/FileGen and ~/FileGeneration)
     # We have to search twice, once for the precise collection and another with

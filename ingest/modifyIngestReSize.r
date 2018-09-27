f1277c465a93f04115bca880d0cbab0b6e352c4a
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2018-09-27 15:37:01 +0200

    Fix for SYS_HEADER_LENGTH error in create-ingest rule (RITDEV-499)

--- ingest/modifyIngestReSize.r
+++ ingest/modifyIngestReSize.r
@@ -23,6 +23,7 @@ modifyIngestReSize() {
     msiWriteRodsLog("Prevent SYS_HEADER_READ_LENGTH and SYS_HEADER_WRITE_LEN_ERR at approximately 16kB ruleset", 0);
     msiWriteRodsLog("Prevent SYS_HEADER_READ_LENGTH and SYS_HEADER_WRITE_LEN_ERR at approximately 16kB ruleset", 0);
     msiWriteRodsLog("Prevent SYS_HEADER_READ_LENGTH and SYS_HEADER_WRITE_LEN_ERR at approximately 16kB ruleset", 0);
+    msiWriteRodsLog("Prevent SYS_HEADER_READ_LENGTH and SYS_HEADER_WRITE_LEN_ERR at approximately 16kB ruleset", 0);
 }
 
 INPUT null

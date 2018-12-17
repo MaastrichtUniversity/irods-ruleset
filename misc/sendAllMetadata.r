7be2fd5a948a77137e357140b2ead6af2a1089f5
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2018-12-17 12:03:48 +0100

    Extra check for metadata.xml in subcollection of P0000X/C0000X (RITDEV-565)

--- misc/sendAllMetadata.r
+++ misc/sendAllMetadata.r
@@ -7,11 +7,18 @@ sendAllMetadata() {
     msi_getenv("MIRTH_METADATA_CHANNEL", *mirthMetaDataUrl);
 
     foreach ( *Row in select COLL_NAME, COLL_PARENT_NAME, DATA_NAME where COLL_NAME like '/nlmumc/projects/%/%' and DATA_NAME = "metadata.xml") {
-        uuChopPath(*Row.COLL_NAME, *head, *collection);
-        uuChopPath(*Row.COLL_PARENT_NAME, *head, *project);
+        if ( *Row.COLL_PARENT_NAME like regex "/nlmumc/projects/P[0-9]{9}$" ) {
+            # metadata.xml at root folder of collection
+            uuChopPath(*Row.COLL_NAME, *head, *collection);
+            uuChopPath(*Row.COLL_PARENT_NAME, *head, *project);
 
-        writeLine("stdout", "Sending " ++ *project ++ " " ++ *collection);
-        sendMetadata(*mirthMetaDataUrl, *project, *collection);
+            writeLine("stdout", "Sending " ++ *project ++ " " ++ *collection);
+            sendMetadata(*mirthMetaDataUrl, *project, *collection);
+        } else {
+            # metadata.xml in a sub collection. We don't want to send that for indexing
+            *subcoll = *Row.COLL_NAME
+            writeLine("stdout", "NOT sending subcollection *subcoll");
+        }
     }
 }
 

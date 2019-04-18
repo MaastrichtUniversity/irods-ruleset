5fd76629d34281dbb433ea66f77f46c63a11c556
Paul van Schayck <polleke@gmail.com>
2019-04-18 11:55:34 +0200

    Call python rules through irods rule language RITDEV-626

--- python/getJSONfromObj.r
+++ python/getJSONfromObj.r
@@ -1,5 +1,5 @@
 # This rule return a json string from AVU's set to an object
-# Argument 1: The object (/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld, /nlmumc/projects/P000000003/C000000001/, user@mail.com, demoResc
+# Argument 1: The object (/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld, /nlmumc/projects/P000000003/C000000001/, user@mail.com, demoResc)
 # Argument 2: The object type -d for data object
 #                             -R for resource
 #                             -C for collection
@@ -7,20 +7,19 @@
 # Argument 3:  the JSON root according to https://github.com/MaastrichtUniversity/irods_avu_json.
 #              if empty string is supplied the irods_avu_json parsing is skipped and a json representing a array of avu's is returned
 
-# Example : irule -r irods_rule_engine_plugin-python-instance -F getJSONfromObj.r "*object='/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld'" "*inputType='-d'" "*jsonRoot='root'"
+# Example : irule -F getJSONfromObj.r "*object='/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld'" "*inputType='-d'" "*jsonRoot='root'"
 
 
-def main(rule_args, callback, rei):
-    #load input variables
-    object = global_vars["*object"][1:-1]
-    input_type = global_vars["*inputType"][1:-1]  
-    json_root = global_vars["*jsonRoot"][1:-1]
+main(){
+    # Defining result first is mandatory!
+    *result = ""
 
-    #Make call to function in core.py
-    ret_val = callback.getJSONfromObj(object, input_type, json_root, "")
-    #Write output to stdout
-    callback.writeLine("stdout", ret_val['arguments'][3])
+    # Call the python function
+    getJSONfromObj(*object, *objectType, *jsonRoot, *result)
 
+    # Print
+    writeLine("stdout", *result)
+}
 
-INPUT *object = '/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld', *inputType = '-d', *jsonRoot = 'root'
+INPUT *object = '/nlmumc/projects/P000000003/C000000001/metadata_cedar.jsonld', *objectType = '-d', *jsonRoot = 'root'
 OUTPUT ruleExecOut
--- python/setJSONtoObj.r
+++ python/setJSONtoObj.r
@@ -8,20 +8,14 @@
 # Argument 4:  the JSON string (make sure the quotes are escaped)  {\"k1\":\"v1\",\"k2\":{\"k3\":\"v2\",\"k4\":\"v3\"},\"k5\":[\"v4\",\"v5\"],\"k6\":[{\"k7\":\"v6\",\"k8\":\"v7\"}]}
 #            
 
-# Example : irule -r irods_rule_engine_plugin-python-instance -F setJSONtoObj.r "*object='/nlmumc/projects/P000000003/C000000001/metadata.xml'" "*inputType='-d'" "*jsonRoot='root'" "*jsonString='{\"k1\":\"v1\",\"k2\":{\"k3\":\"v2\",\"k4\":\"v3\"},\"k5\":[\"v4\",\"v5\"],\"k6\":[{\"k7\":\"v6\",\"k8\":\"v7\"}]}'"
+# Example : irule -F setJSONtoObj.r "*object='/nlmumc/projects/P000000003/C000000001/metadata.xml'" "*inputType='-d'" "*jsonRoot='root'" "*jsonString='{\"k1\":\"v1\",\"k2\":{\"k3\":\"v2\",\"k4\":\"v3\"},\"k5\":[\"v4\",\"v5\"],\"k6\":[{\"k7\":\"v6\",\"k8\":\"v7\"}]}'"
 
 
-def main(rule_args, callback, rei):
-    # load input variables
-    object = global_vars["*object"][1:-1]
-    input_type = global_vars["*inputType"][1:-1]
-    json_root = global_vars["*jsonRoot"][1:-1]
-    json_string = global_vars["*jsonString"][1:-1]
-
+main() {
     # Make call to function in core.py
-    callback.setJSONtoObj(object, input_type, json_root ,json_string )
+    setJSONtoObj(*object, *objectType, *jsonRoot, *jsonString)
+}
 
-
-INPUT *object = '/nlmumc/projects/P000000003/C000000001/metadata.xml', *inputType = '-d', *jsonRoot = 'root', *jsonString='{\"k1\":\"v1\",\"k2\":{\"k3\":\"v2\",\"k4\":\"v3\"},\"k5\":[\"v4\",\"v5\"],\"k6\":[{\"k7\":\"v6\",\"k8\":\"v7\"}]}'
+INPUT *object = '/nlmumc/projects/P000000003/C000000001/metadata.xml', *objectType = '-d', *jsonRoot = 'root', *jsonString='{\"k1\":\"v1\",\"k2\":{\"k3\":\"v2\",\"k4\":\"v3\"},\"k5\":[\"v4\",\"v5\"],\"k6\":[{\"k7\":\"v6\",\"k8\":\"v7\"}]}'
 OUTPUT ruleExecOut
 

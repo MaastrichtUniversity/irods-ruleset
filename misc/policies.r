c6a95cc4c5ac0ee06eba58e8ea19a51a0e376a15
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2019-05-28 12:33:45 +0200

    Reset the use of multi-threading when using non-Ceph-S3 resources (RITDEV-498)

--- misc/policies.r
+++ misc/policies.r
@@ -104,9 +104,14 @@ acPreProcForModifyAVUMetadata(*Option,*SourceItemType,*TargetItemType,*SourceIte
 }
 
 # Enforce single threaded iRODS data connections when using Ceph S3 resources
+# The arguments for msiSetNumThreads are:
+# 1) sizePerThrInMb - integer value in MBytes to calculate the number of threads (default: 32)
+# 2) maxNumThr      - the maximum number of threads to use (default: 4).
+# 3) windowSize     - the tcp window size in Bytes for the parallel transfer (default: 1048576).
 acSetNumThreads {
     if ($KVPairs.rescName == "UM-Ceph-S3-AC" || $KVPairs.rescName == "UM-Ceph-S3-GL") {
         msiSetNumThreads("default","0","default");
+    } else {
+        msiSetNumThreads("default","16","default");
     }
 }
-

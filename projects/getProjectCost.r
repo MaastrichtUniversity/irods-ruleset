34439a2ced0ad908eef15a5b167d1d287d40c4d7
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2019-07-24 15:21:57 +0200

    Various updates for (RITDEV-653) - use RESC_ID instead of RESC_NAME - regex on bytesize_resc now checks on digits only - sizeOnResource calculated and displayed in GB instead of GiB - Error messages if certain AVUs cannot be found

--- projects/getProjectCost.r
+++ projects/getProjectCost.r
@@ -34,32 +34,39 @@ IRULE_getProjectCost(*project, *result, *collections) {
         *resourceDetails = '[]';
         *detailsArraySize = 0;
         *collectionCost = 0;
+        *resourceId = "";
 
-        foreach (*av in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME == "*projectCollection") {
+        foreach (*av in SELECT META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE WHERE COLL_NAME == "*projectCollection" ) {
             # Check dcat:byteSize AVU for each resource
-            if ( *av.META_COLL_ATTR_NAME like regex "dcat:byteSize_(.+)") {
+            if ( *av.META_COLL_ATTR_NAME like regex "dcat:byteSize_resc_([0-9])+" ) {
                 *value = *av.META_COLL_ATTR_VALUE;
                 *name = *av.META_COLL_ATTR_NAME;
+                *resourceId = triml(*name, "resc_");
 
-                # GiB
-                *sizeOnResource = double(*value)/1024/1024/1024;
-                *resourceName = triml(*name, "_");
+                # Convert to GB
+                *sizeOnResource = double(*value)/1000/1000/1000;
 
+                # Lookup the price for this resource
                 *pricePerGBPerYearStr = "";
-                # Update pricePerGBPerYearStr with resource's price if no specialPrice is present
-
-                foreach (*av in SELECT META_RESC_ATTR_NAME, META_RESC_ATTR_VALUE WHERE RESC_NAME == "*resourceName") {
+                foreach (*av in SELECT META_RESC_ATTR_NAME, META_RESC_ATTR_VALUE WHERE RESC_ID== "*resourceId") {
                     if ( *av.META_RESC_ATTR_NAME == *pricePerGBPerYearAttr) {
                         *pricePerGBPerYearStr = *av.META_RESC_ATTR_VALUE;
                     }
+                }
+                if ( *pricePerGBPerYearStr == "" ) {
+                    failmsg(-1, "Resource ID '*resourceId': no attribute called '*pricePerGBPerYearAttr' found");
                 }
 
                 *storageCostOnResc = *sizeOnResource * double(*pricePerGBPerYearStr);
                 *collectionCost = *collectionCost + *storageCostOnResc;
 
-                *details = '{"resource": "*resourceName", "dataSizeGiB": "*sizeOnResource", "pricePerGBPerYear": "*pricePerGBPerYearStr", "storageCostOnResource": "*storageCostOnResc"}';
+                *details = '{"resource": "*resourceId", "dataSizeGBOnResource": "*sizeOnResource", "pricePerGBPerYear": "*pricePerGBPerYearStr", "storageCostOnResource": "*storageCostOnResc"}';
                 msi_json_arrayops(*resourceDetails, *details, "add", *detailsArraySize);
             }
+        }
+        # Error out if no byteSize_resc attribute is present for this collection
+        if ( *resourceId == "" ) {
+            failmsg(-1, "*projectCollection: no attribute 'dcat:byteSize_resc_<RescID>' found");
         }
 
         *collId = triml("*projectCollection","*project"++"/");

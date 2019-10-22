cc39b065555bbed7599d90cc1ba5936f26530369
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2019-10-22 16:32:30 +0200

    Added support for non-replication resources (i.e. resources that are not in a hierarchy, i.e. orphans) (RITDEV-516, RITDEV-653)

--- misc/calcCollectionSizeAcrossCoordResources.r
+++ misc/calcCollectionSizeAcrossCoordResources.r
@@ -14,30 +14,64 @@
 # Output:
 # This rule outputs the same information both in json and iRODS list format
 
+# TODO: Rename this rule, as it now supports all resources (coordinating + orphans)
+# TODO: Rename the calcCollectionFilesAcross... rule, as it now supports all resources (coordinating + orphans)
+# TODO: Put the resource type determination in a separate rule, that will be called from here and the calcCollectionFiles
+
 irule_dummy() {
     IRULE_calcCollectionSizeAcrossCoordResources(*collection, *unit, *round, *result, *resultIdList, *resultSizeList);
     writeLine("stdout", *result);
 }
 
 IRULE_calcCollectionSizeAcrossCoordResources(*collection, *unit, *round, *result, *resultIdList, *resultSizeList) {
-    *resources = list();
-
-    # Determine all resources used in this collection
-    foreach ( *Row in SELECT RESC_PARENT WHERE COLL_NAME like "*collection%") {
-        *resources = cons(*Row.RESC_PARENT, *resources);
-    }
-
+    # Initialize variables
+    *resources; # Key pair Value object to store resource type for each resource. key -> resource ID; value ->  "parent" or "orphan"
     *rescSizeArray = '[]';
     *rescIdList = list();
     *rescSizeList = list();
 
-    # Loop over resources list
-    foreach ( *resc in *resources ) {
+    # Determine all parent resources (= coordResc) used in this collection
+    # Some resources do not have a parent and return an empty string. These are excluded from the SQL result
+    foreach ( *Row in SELECT RESC_PARENT WHERE RESC_PARENT != "" and COLL_NAME like "*collection%") {
+        # reset *id and *type
+        *id = 0
+        *type = ""
+
+        # Add values to resources kvp
+        *id = *Row.RESC_PARENT;
+        *type = "parent";
+        *resources.*id = *type;
+    }
+
+    # Repeat for orphan resources (parentless)
+    foreach ( *Row in SELECT RESC_ID WHERE RESC_PARENT == "" and COLL_NAME like "*collection%") {
+        # reset *id and *type
+        *id = 0
+        *type = ""
+
+        # Add values to resources kvp
+        *id = *Row.RESC_ID;
+        *type = "orphan";
+        *resources.*id = *type;
+    }
+#    writeLine("stdout", *resources);
+#    returns KeyValue[3]:10002=parent;60742=orphan;83598=orphan;
+
+    # Loop over the resources key-value object
+    foreach ( *rescId in *resources ) {
         *sizeBytes = 0;
 
         # Loop over and sum the size of all distinct files in this collection-resource combination
-        foreach ( *Row in SELECT DATA_ID, DATA_SIZE WHERE COLL_NAME like "*collection%" and RESC_PARENT = *resc) {
-            *sizeBytes = *sizeBytes + double(*Row.DATA_SIZE);
+        if ( *resources.*rescId == "parent" ) { # if the value of *rescId equals parent
+            foreach ( *Row in SELECT DATA_ID, DATA_SIZE WHERE COLL_NAME like "*collection%" and RESC_PARENT = *rescId) {
+                *sizeBytes = *sizeBytes + double(*Row.DATA_SIZE);
+            }
+        } else if ( *resources.*rescId == "orphan" ) { # if the value of *rescId equals orphan
+            foreach ( *Row in SELECT DATA_ID, DATA_SIZE WHERE COLL_NAME like "*collection%" and RESC_ID = *rescId) {
+                *sizeBytes = *sizeBytes + double(*Row.DATA_SIZE);
+            }
+        } else {
+            failmsg(-1, "Error with determining storage resource type");
         }
 
         # Convert to different unit
@@ -72,11 +106,11 @@ IRULE_calcCollectionSizeAcrossCoordResources(*collection, *unit, *round, *result
         }
 
         # Collect the values for this iteration and add it to the json array
-        *jsonArr = '{"resourceID": "*resc", "dataSize": "*roundedSize"}';
+        *jsonArr = '{"resourceID": "*rescId", "dataSize": "*roundedSize"}';
         msi_json_arrayops(*rescSizeArray, *jsonArr, "add", 0);
 
         # Add the same values to the list object
-        *rescIdList = cons(*resc, *rescIdList);
+        *rescIdList = cons(*rescId, *rescIdList);
         *rescSizeList = cons(*roundedSize, *rescSizeList);
     }
 

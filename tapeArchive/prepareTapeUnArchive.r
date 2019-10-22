d7fdb90cbeb2797e0084f23b3ee61974947976df
JonathanMELIUS <meliusjonathan@gmail.com>
2019-10-22 11:26:26 +0200

    Fix state avu check & Update state avu messages (RITDEV-516)

--- tapeArchive/prepareTapeUnArchive.r
+++ tapeArchive/prepareTapeUnArchive.r
@@ -16,7 +16,8 @@ IRULE_prepareTapeUnArchive(*archColl) {
     }
 
     # Check for valid state to start archiving
-    if ( *archiveState != "no-state-set" && *archiveState != "archive-done"  && *archiveState != "") {
+    if ( *archiveState != "no-state-set" && *archiveState != "archive-done"  && *archiveState != ""
+            && *archiveState not like  "Number of files offline:*"  && *archiveState not like  "Caching files countdown:*") {
         failmsg(-1, "Invalid state(*archiveState) to start process.");
     }
 
@@ -32,11 +33,11 @@ IRULE_prepareTapeUnArchive(*archColl) {
 
     msiGetObjType(*archColl, *inputType);
     if (*inputType like '-d'){
-         writeLine("stdout","Checking data status: *archColl");
+         writeLine("serverLog", "Checking data status: *archColl");
         *count = checkTapeFile(*resc, *svr, *archColl, *dmfs_attr, *dataPathList);
     }
     if (*inputType like '-c'){
-        writeLine("stdout","Checking collection status: *archColl");
+        writeLine("serverLog", "Checking collection status: *archColl");
         *count = checkTapeCollection(*resc, *svr, *archColl, *dmfs_attr, *dataPathList);
     }
 
@@ -46,7 +47,7 @@ IRULE_prepareTapeUnArchive(*archColl) {
     # Open collection to update status AVU *archiveState
     openProjectCollection(*project, *projectCollection, *aclChange, 'own');
 
-    *value = "Number of files to bring back online: *count";
+    *value = "Number of files offline: *count";
     setCollectionAVU(*archColl, *stateAttrName, *value)
 
     *unMigratingCounter = 0;
@@ -98,8 +99,8 @@ IRULE_prepareTapeUnArchive(*archColl) {
 
     # UnMigrating Check
     if (*unMigratingList != ""){
-        writeLine("serverLog", "Unmagrating to cache: *unMigratingList");
-        *value = "*unMigratingCounter";
+        writeLine("serverLog", "Un-migrating to cache: *unMigratingList");
+        *value = "Caching files countdown: *unMigratingCounter";
         setCollectionAVU(*archColl, *stateAttrName, *value)
 
         # Delay & recursive call

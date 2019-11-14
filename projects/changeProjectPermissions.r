c376dd714c70499c563176ce6c1e8c1d8808e37b
Paul van Schayck <polleke@gmail.com>
2019-11-14 15:41:41 +0100

    Simplified rule. Fixed bug where empty user string was giving error. RITDEV-670

--- projects/changeProjectPermissions.r
+++ projects/changeProjectPermissions.r
@@ -1,6 +1,6 @@
 # Call with
 #
-# irule -s -F changeProjectPermissions.r *project="P000000016" *users="rit-l:read"
+# irule -s -F changeProjectPermissions.r *project="P000000016" *users="p.vanschayck@maastrichtuniversity.nl:read m.coonen@maastrichtuniversity.nl:write"
 #
 # Change immediately the ACL on the project level.
 # Then in the delay queue, change recursively all the collections under the project.
@@ -13,26 +13,12 @@ IRULE_changeProjectPermissions(*project, *users){
     # Save users list before uuChop
     *input_users = *users;
 
-    # Change ACL on the project level
-    uuChop(*users, *head, *tail, " ", true);
-    if (*head != ""){
-        # Case where users contains multiple inputs "account:right"
-        # Execute first uuChop result
-        uuChop(*head, *account, *rights, ":", true);
-        msiSetACL("default", "*rights", "*account", '/nlmumc/projects/*project');
-    }
-    else{
-        # Case where users contains only one input account:right
-        uuChop(*users, *account, *rights, ":", true);
-        msiSetACL("default", "*rights", "*account", '/nlmumc/projects/*project');
-    }
+    # Infinite loop safe-guard. Protects again malformed input.
+    # This is the maximum of permission changes that can be made
+    *count = 100;
 
-    # Infinite loop safe-guard
-    *count = 25;
-
-    # Handle the rest of uuChop result
-    *users = *tail;
-    while ( *head != "" && *count != 0){
+    # Change ACL on the project level by chopping up the input array on space, and looping over it
+    while ( *users != "" && *count != 0){
         uuChop(*users, *head, *tail, " ", true);
         if (*head != ""){
             uuChop(*head, *account, *rights, ":", true);
@@ -56,41 +42,28 @@ IRULE_changeProjectPermissions(*project, *users){
             # Open the collection to be able to modify the collection ACL
             msiSetACL("recursive", "admin:own", "rods", "*projectCollection");
 
-            # Change collection ACL recursively
-            uuChop(*delay_users, *head, *tail, " ", true);
-            *collection_rights = "read";
-            if (*head != ""){
-                uuChop(*head, *account, *rights, ":", true);
-                if (*rights == "null"){
-                    *collection_rights = "null";
-                }
-                msiSetACL("recursive", "*collection_rights", "*account", "*projectCollection");
-            }
-            else{
-                uuChop(*delay_users, *account, *rights, ":", true);
-                if (*rights == "null"){
-                    *collection_rights = "null";
-                }
-                msiSetACL("recursive", "*collection_rights", "*account", "*projectCollection");
-            }
-            *delay_users = *tail;
-
-            while ( *head != ""){
+            # Change ACL on the projectCollection level by chopping up the input array on space, and looping over it
+            while (*delay_users != "") {
                 uuChop(*delay_users, *head, *tail, " ", true);
                 if (*head != ""){
                     uuChop(*head, *account, *rights, ":", true);
+
+                    # Always set rights to read, unless they are removed
                     *collection_rights = "read";
                     if (*rights == "null"){
                         *collection_rights = "null";
                     }
                     msiSetACL("recursive", "*collection_rights", "*account", "*projectCollection");
                 }
-                if (*tail != ""){
+                else {
                     uuChop(*tail, *account, *rights, ":", true);
+
+                    # Always set rights to read, unless they are removed
                     *collection_rights = "read";
                     if (*rights == "null"){
                         *collection_rights = "null";
                     }
+
                     msiSetACL("recursive", "*collection_rights", "*account", "*projectCollection");
                 }
                 *delay_users = *tail;

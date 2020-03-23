10749711621ce52a3a96c844f69f16d056b2bbe2
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2020-03-23 16:49:16 +0100

    Changes in usage instructions [DHS-2 DHS-9]

--- projectCollection/requestExportProjectCollection.r
+++ projectCollection/requestExportProjectCollection.r
@@ -1,9 +1,10 @@
 # Call with
-# irule -s -F requestExportProjectCollection.r *message='{"project": "P000000001", "collection": "C000000012"}' \
+# irule -s -F requestExportProjectCollection.r *message='{"project": "P000000014", "collection": "C000000001"}' \
 #   *project='P000000014'  *collection='C000000001' *repository='Dataverse' \
-#   *amqpHost='rabbitmq' *amqpPort=5672 *amqpUser='user'  *amqpPass='password'
+#   *amqpHost='rabbitmq' *amqpPort=5672 *amqpUser='user' *amqpPass='password'
 #
-# Create exporterState AVU and send the message to the queue to be processed
+# This rule creates an AVU called 'exporterState' and sends the message to the amqp queue to be processed by another application
+# Make sure to call this rule as 'rodsadmin' because it will open a collection using admin-mode.
 
 irule_dummy() {
     IRULE_requestExportProjectCollection(*message, *project, *collection, *repository, *amqpHost, *amqpPort, *amqpUser, *amqpPass);
@@ -19,7 +20,7 @@ IRULE_requestExportProjectCollection(*message, *project, *collection, *repositor
     msiAssociateKeyValuePairsToObj(*metaKV, "/nlmumc/projects/*project/*collection", "-C");
 
     # Send the message to the queue and it will be processed by the open access ETL worker
-    # msi_amqp_basic_publish(hostname, port, user, password, exchange, routing_key, message)
+    # Arguments: msi_amqp_basic_publish(hostname, port, user, password, exchange, routing_key, message)
     msi_amqp_basic_publish(*amqpHost, int(*amqpPort), *amqpUser, *amqpPass, "datahub.events_tx", "projectCollection.exporter.requested", *message );
 }
 

cf95c647235238db68fe8f1d78067e4d3f1ae1a7
Sebastiaan <s.nijhuis@maastrichtuniversity.nl>
2020-04-07 11:34:22 +0200

    Added rule to get groupmemberships for a given user (DHS-144)

---
+++ misc/getGroupMemberships.r
@@ -0,0 +1,30 @@
+# Call with
+#
+# irule -F getGroups.r "*showSpecialGroups='true'"
+# irule -F getGroupMemberships.r "*user='name'" (where name is like name@maastrichtuniversity.nl
+
+irule_dummy() {
+    IRULE_getGroupMemberships(*user, *groupList);
+}
+
+IRULE_getGroupMemberships(*user, *groupList) {
+	userNameToUserId(*user, *userId);
+	
+	*groups = "";
+	foreach (*row in SELECT USER_GROUP_NAME, USER_GROUP_ID
+				WHERE USER_ID = '*userId') {
+		msiGetValByKey(*row,"USER_GROUP_NAME",*group);
+		# workasround needed: iRODS returns username also as a group !!
+		if (*group != *user) {
+			*groups = "*groups:*group";
+		}
+	}
+	*groups = triml(*groups,":");
+	*groupList = split(*groups, ":");
+
+	writeLine("stdout", "*groupList");
+
+}
+
+INPUT *user='rods'
+OUTPUT ruleExecOut

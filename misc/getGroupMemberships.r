5f58fe318bca61bdcc0035ef35854e6ea23bffc2
Jan-Erek Thiede <j.thiede@maastrichtuniversity.nl>
2020-04-12 22:44:45 +0200

    rule getGroupMemeberships filters special groups [DHS-126, DHS-144]

--- misc/getGroupMemberships.r
+++ misc/getGroupMemberships.r
@@ -3,26 +3,27 @@
 # irule -F getGroupMemberships.r "*user='name'" (where name is like name@maastrichtuniversity.nl
 
 irule_dummy() {
-    IRULE_getGroupMemberships(*user, *groupList);
+    IRULE_getGroupMemberships(*user, *result);
 }
 
-IRULE_getGroupMemberships(*user, *groupList) {
+IRULE_getGroupMemberships(*user, *result) {
+        *userId = "";
 	userNameToUserId(*user, *userId);
-	
+	writeLine("serverLog", "getting groupMemeberships for userName: *user, userId: *userId" );
 	*groups = "";
 	foreach (*row in SELECT USER_GROUP_NAME, USER_GROUP_ID
-				WHERE USER_ID = '*userId') {
-		msiGetValByKey(*row,"USER_GROUP_NAME",*group);
-		# workasround needed: iRODS returns username also as a group !!
-		if (*group != *user) {
-			*groups = "*groups:*group";
+				WHERE USER_ID = '*userId' ) {
+                if ( *row.USER_GROUP_NAME != "public" &&  *row.USER_GROUP_NAME != "rodsadmin" && *row.USER_GROUP_NAME != "DH-ingest" && *row.USER_GROUP_NAME != "DH-project-admins" ) {
+		   msiGetValByKey(*row,"USER_GROUP_NAME",*group);
+		   # workasround needed: iRODS returns username also as a group !!
+		   if (*group != *user) {
+                      *groups = "*groups:*group"; 
+                   }
 		}
 	}
 	*groups = triml(*groups,":");
-	*groupList = split(*groups, ":");
-
-	writeLine("stdout", "*groupList");
-
+	*result = split(*groups, ":");
+	writeLine("serverLog", "user is member of: *result");
 }
 
 INPUT *user='rods'

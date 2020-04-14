69dd68459b79f82fbc988d2d3204b9a07c99cf48
Jan-Erek Thiede <j.thiede@maastrichtuniversity.nl>
2020-04-14 12:41:29 +0200

    work in progress. Rule getGroupMemberships returns an array [DHS-126, DHS-140]

--- misc/getGroupMemberships.r
+++ misc/getGroupMemberships.r
@@ -9,20 +9,23 @@ irule_dummy() {
 IRULE_getGroupMemberships(*user, *result) {
         *userId = "";
 	userNameToUserId(*user, *userId);
-	writeLine("serverLog", "getting groupMemeberships for userName: *user, userId: *userId" );
-	*groups = "";
+	writeLine("serverLog", "getting groupMemberships for userName: *user, userId: *userId" );
+	*groups = '[]';
+        *groupsSize = 0;
 	foreach (*row in SELECT USER_GROUP_NAME, USER_GROUP_ID
 				WHERE USER_ID = '*userId' ) {
                 if ( *row.USER_GROUP_NAME != "public" &&  *row.USER_GROUP_NAME != "rodsadmin" && *row.USER_GROUP_NAME != "DH-ingest" && *row.USER_GROUP_NAME != "DH-project-admins" ) {
 		   msiGetValByKey(*row,"USER_GROUP_NAME",*group);
 		   # workasround needed: iRODS returns username also as a group !!
 		   if (*group != *user) {
-                      *groups = "*groups:*group"; 
+                      #*groups = "*groups:*group";
+                      msi_json_arrayops(*groups, *group, "add", *groupsSize); 
                    }
 		}
 	}
-	*groups = triml(*groups,":");
-	*result = split(*groups, ":");
+	#*groups = triml(*groups,":");
+	#*result = split(*groups, ":");
+        *result = *groups;
 	writeLine("serverLog", "user is member of: *result");
 }
 

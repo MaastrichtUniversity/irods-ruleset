ff3612260256c24a5b478ef2d48e5c8d522fbdfd
Jan-Erek Thiede <j.thiede@maastrichtuniversity.nl>
2020-04-18 23:45:02 +0200

    rules getGroupMemberships and getUsersInGroup return id and name as attributes [DHS-126, DHS-140]

--- misc/getGroupMemberships.r
+++ misc/getGroupMemberships.r
@@ -7,24 +7,24 @@ irule_dummy() {
     writeLine("stdout", *result);
 }
 
-IRULE_getGroupMemberships(*user, *result) {
+IRULE_getGroupMemberships(*userName, *result) {
     *userId = "";
-	userNameToUserId(*user, *userId);
-	writeLine("serverLog", "getting groupMemberships for userName: *user, userId: *userId" );
-	*groups = '[]';
-    *groupsSize = 0;
-	foreach (*row in SELECT order(USER_GROUP_NAME), USER_GROUP_ID
-        WHERE USER_ID = '*userId' ) {
-        if ( *row.USER_GROUP_NAME != "public" &&  *row.USER_GROUP_NAME != "rodsadmin" && *row.USER_GROUP_NAME != "DH-ingest" && *row.USER_GROUP_NAME != "DH-project-admins" ) {
-            *group = *row.USER_GROUP_NAME;
-            # workasround needed: iRODS returns username also as a group !!
-            if (*group != *user) {
-                msi_json_arrayops(*groups, *group, "add", *groupsSize);
-            }
-        }
-	}
-    *result = *groups;
-	writeLine("serverLog", "user is member of: *result");
+    userNameToUserId(*userName, *userId);
+    #writeLine("serverLog", "getting groupMemberships for userName: *user, userId: *userId" );
+    *result = '[]';
+    *resultSize = 0;
+    foreach (*row in SELECT order(USER_GROUP_NAME), USER_GROUP_ID
+                     WHERE USER_ID = '*userId' ) {
+       if ( *row.USER_GROUP_NAME != "public" &&  *row.USER_GROUP_NAME != "rodsadmin" && *row.USER_GROUP_NAME != "DH-ingest" && *row.USER_GROUP_NAME != "DH-project-admins" ) {
+          *groupName = *row.USER_GROUP_NAME;
+          *groupId = *row.USER_GROUP_ID;
+          # workasround needed: iRODS returns username also as a group !!
+          if (*groupName != *userName) {
+             *groupJSON = '{ "groupId": "*groupId", "name" : "*groupName" }';
+              msi_json_arrayops( *result, *groupJSON, "add", *resultSize);
+          }
+       }
+    }
 }
 
 INPUT *user='rods'
--- misc/getUsersInGroup.r
+++ misc/getUsersInGroup.r
@@ -3,24 +3,22 @@
 # irule -F getUsersInGroup.r "*group='name'"
 
 irule_dummy() {
-    IRULE_getUsersInGroup(*group, *result);
+    IRULE_getUsersInGroup(*groupId, *result);
     writeLine("stdout", *result);
 }
 
-IRULE_getUsersInGroup(*group, *result) {
-    writeLine("serverLog", "searching users in group: *group");
-	
-	*users = "[]";
-    *usersSize = 0;
-    foreach ( *row in select order(USER_NAME) where USER_TYPE = 'rodsuser' and USER_GROUP_NAME = *group ) {
-        *user = *row.USER_NAME;
-        msi_json_arrayops(*users, *user, "add", *usersSize);
-	}
-    *result = *users;
-
-	writeLine("serverLog", "found users: *result");
+IRULE_getUsersInGroup(*groupId, *result) {
+ #   writeLine("serverLog", "searching users in group: *group");	
+    *result = "[]";
+    *resultSize = 0;
+    foreach ( *row in select order(USER_NAME), USER_ID where USER_TYPE = 'rodsuser' and USER_GROUP_ID = *groupId ) {
+        *displayName = *row.USER_NAME;
+        *userId = *row.USER_ID;
+        *userJSON = '{ "userId": "*userId", "displayName" : "*displayName" }';
+        msi_json_arrayops(*result, *userJSON, "add", *resultSize);
+    }
 }
 
 
-INPUT *group=''
+INPUT *groupId=''
 OUTPUT ruleExecOut

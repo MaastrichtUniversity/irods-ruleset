7ca31628d382ed56191d78b45ffd682e2f6cbee3
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2020-06-30 15:53:29 +0200

    Typos and whitespace [DHS-117, DHS-492]

--- misc/getDisplayNameForUserEmail.r
+++ misc/getDisplayNameForUserEmail.r
@@ -11,22 +11,22 @@ IRULE_getDisplayNameForUserEmail(*email,*result) {
     *userName = ""
     *userDisplayName = ""
 
-     # First try to find an irods user with a username equal to the email address provided
+     # First try to find an irods account with a username equal to the email address provided
      foreach (*Row in SELECT USER_NAME WHERE USER_NAME == "*email"  ) {
-                 *userName = *Row.USER_NAME
+            *userName = *Row.USER_NAME
      }
-     # If the user_name does not exist try to find a user with a matching email address
+     # If the username does not exist try to find a user with a matching email address
      if (*userName == "") {
               foreach (*Row in SELECT USER_NAME WHERE META_USER_ATTR_VALUE == "*email" AND META_USER_ATTR_NAME == "email" ) {
-                        *userName = *Row.USER_NAME
+                    *userName = *Row.USER_NAME
               }
               # If username is still empty the user cannot be found in the system
               if (*userName == "") {
-                               failmsg(-1, "ERROR: A user with email adress '*email' was not found!");
+                    failmsg(-1, "ERROR: A user with email address '*email' was not found!");
               }
       }
-     # Get the isdplay name givenb the username
-     foreach (*Row in SELECT META_USER_ATTR_VALUE WHERE  USER_NAME == "*userName" AND META_USER_ATTR_NAME == "displayName" ) {
+     # Get the display name given the username
+     foreach (*Row in SELECT META_USER_ATTR_VALUE WHERE USER_NAME == "*userName" AND META_USER_ATTR_NAME == "displayName" ) {
             *userDisplayName = *Row.META_USER_ATTR_VALUE
      }
      # If the Display name is missing revert to the email

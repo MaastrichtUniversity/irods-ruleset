71a2c0dd3f4de2cbd32da1cd25025526b9ef111f
jonathanMELIUS <meliusjonathan@gmail.com>
2020-06-30 15:00:39 +0200

    Optimize queries in getDataStewards.r [DHS-117, DHS-487]

--- misc/getDataStewards.r
+++ misc/getDataStewards.r
@@ -10,19 +10,30 @@ irule_dummy() {
 IRULE_getDataStewards(*result) {
     *userObjects = '[]';
     *userObjectsSize = 0;
-    
-    foreach (*Row in SELECT USER_ID WHERE  META_USER_ATTR_VALUE == "data-steward" AND META_USER_ATTR_NAME == "specialty") {
+
+    *users = ""
+    foreach (*Row in SELECT USER_ID WHERE META_USER_ATTR_VALUE == "data-steward" AND META_USER_ATTR_NAME == "specialty") {
         *userID = *Row.USER_ID;
 
-        foreach (*O in SELECT USER_NAME, META_USER_ATTR_VALUE WHERE USER_ID == "*userID" AND META_USER_ATTR_NAME == "displayName") {
-            *userName = *O.USER_NAME;
-            *value = *O.META_USER_ATTR_VALUE;
-
-            *userObject = '{ "userName" : "*userName", "userId" : "*userID", "displayName" : "*value" }';
-            msi_json_arrayops( *userObjects, *userObject, "add", *userObjectsSize );
+        # The GenQuery expect the user id to be quoted
+        if (*users == ""){
+            *users = "'" ++ *userID  ++ "'"
+        }
+        else{
+            *users = *users ++ ", '" ++ *userID ++ "'";
         }
     }
+       
+    msiMakeGenQuery("USER_NAME, USER_ID, META_USER_ATTR_VALUE", "USER_ID in (*users) AND META_USER_ATTR_NAME = 'displayName'", *Query);
+    msiExecGenQuery(*Query, *QOut);
+    foreach ( *O in *QOut ) {
+        *userName = *O.USER_NAME;
+        *value = *O.META_USER_ATTR_VALUE;
+        *userID = *O.USER_ID;
 
+        *userObject = '{ "userName" : "*userName", "userId" : "*userID", "displayName" : "*value" }';
+        msi_json_arrayops( *userObjects, *userObject, "add", *userObjectsSize );
+	}
     *result = *userObjects;
 }
 

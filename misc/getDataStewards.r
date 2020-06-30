6af2f2679505c7d7e3a05663bce34483ded91a2b
JonathanMELIUS <meliusjonathan@gmail.com>
2020-06-30 12:32:58 +0200

    Update getDataStewards.r to return displayName [DHS-117, DHS-487]

--- misc/getDataStewards.r
+++ misc/getDataStewards.r
@@ -8,14 +8,22 @@ irule_dummy() {
 }
 
 IRULE_getDataStewards(*result) {
-    *users = '[]';
-    *usersSize = 0;
+    *userObjects = '[]';
+    *userObjectsSize = 0;
     
-    foreach (*Row in SELECT USER_NAME WHERE  META_USER_ATTR_VALUE == "data-steward" AND META_USER_ATTR_NAME == "specialty") {    		
-        msi_json_arrayops(*users, *Row.USER_NAME, "add", *usersSize);
+    foreach (*Row in SELECT USER_ID WHERE  META_USER_ATTR_VALUE == "data-steward" AND META_USER_ATTR_NAME == "specialty") {
+        *userID = *Row.USER_ID;
+
+        foreach (*O in SELECT USER_NAME, META_USER_ATTR_VALUE WHERE USER_ID == "*userID" AND META_USER_ATTR_NAME == "displayName") {
+            *userName = *O.USER_NAME;
+            *value = *O.META_USER_ATTR_VALUE;
+
+            *userObject = '{ "userName" : "*userName", "userId" : "*userID", "displayName" : "*value" }';
+            msi_json_arrayops( *userObjects, *userObject, "add", *userObjectsSize );
+        }
     }
 
-    *result = *users;
+    *result = *userObjects;
 }
 
 INPUT null

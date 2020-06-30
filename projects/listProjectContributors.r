1d3e1644064724db61c2a12c7752e890467d55a8
jonathanMELIUS <meliusjonathan@gmail.com>
2020-06-30 12:43:10 +0200

    Update listProject* rules to return json userObjects including displayName [DHS-196, DHS-476]

--- projects/listProjectContributors.r
+++ projects/listProjectContributors.r
@@ -1,6 +1,6 @@
 # Call with
 #
-# irule -F listProjectContributors.r "*project='P000000001'" "*inherited='true'"
+# irule -F listProjectContributors.r "*project='P000000010'" "*inherited='true'"
 #
 # Role inheritance
 # *inherited='true' cumulates authorizations to designate the role. i.e. A contributor has OWN or WRITE access
@@ -23,6 +23,9 @@ IRULE_listProjectContributors(*project, *inherited, *result) {
     *groupObjects = '[]';
     *groupObjectsSize = 0;
 
+    *userObjects = '[]';
+    *userObjectsSize = 0;
+
     if ( *inherited == "true" ) {
         *criteria = "'own', 'modify object'"
     } else {
@@ -35,30 +38,35 @@ IRULE_listProjectContributors(*project, *inherited, *result) {
     foreach ( *Row in *QOut ) {
         *objectID = *Row.COLL_ACCESS_USER_ID;
 
-        *O = select USER_NAME, USER_TYPE where USER_ID = '*objectID';
-
-        *objectName = "";
-        *objectType = "";
+        *O = select USER_NAME, USER_TYPE, META_USER_ATTR_NAME, META_USER_ATTR_VALUE where USER_ID = '*objectID' AND USER_TYPE = "rodsuser";
 
         foreach (*R in *O) {
             *objectName = *R.USER_NAME;
             *objectType = *R.USER_TYPE;
+
+            if ( *R.META_USER_ATTR_NAME == "displayName") {
+                *value = *R.META_USER_ATTR_VALUE;
+                msi_json_arrayops(*users, *objectName, "add", *userSize);
+                *userObject = '{ "userName" : "*objectName", "userId" : "*objectID", "displayName" : "*value" }';
+                msi_json_arrayops( *userObjects, *userObject, "add", *userObjectsSize );
+            }
         }
 
-        if ( *objectType == "rodsgroup" ) {
+        *O = select USER_NAME, USER_TYPE where USER_ID = '*objectID' AND USER_TYPE = "rodsgroup";
+
+        foreach (*R in *O) {
+            *objectName = *R.USER_NAME;
+            *objectType = *R.USER_TYPE;
+
             msi_json_arrayops( *groups, *objectName, "add", *groupSize);
             *groupObject = '{ "groupName" : "*objectName", "groupId" : "*objectID" }';
             msi_json_arrayops( *groupObjects, *groupObject, "add", *groupObjectsSize );
         }
 
-        if ( *objectType == "rodsuser" ) {
-            msi_json_arrayops(*users, *objectName, "add", *userSize);
-        }
-
         # All other cases of objectType, such as "" or "rodsadmin", are skipped
     }
 
-    *result = '{"users": *users, "groups": *groups, "groupObjects": *groupObjects }';
+    *result = '{"users": *users, "groups": *groups, "groupObjects": *groupObjects, "userObjects": *userObjects}';
 }
 
 INPUT *project=$"P00000001", *inherited=""

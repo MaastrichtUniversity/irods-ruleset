577963df24db1ca34958172c429e133ae1a992b1
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2020-09-03 15:53:57 +0200

    Update the getUsersInGroup rule to use user objects [DHS-498,DHS-781]

--- misc/getUsersInGroup.r
+++ misc/getUsersInGroup.r
@@ -1,6 +1,6 @@
 # Call with
 #
-# irule -F getUsersInGroup.r "*group='name'"
+# irule -F getUsersInGroup.r "*groupId='2'"
 
 irule_dummy() {
     IRULE_getUsersInGroup(*groupId, *result);
@@ -11,11 +11,21 @@ IRULE_getUsersInGroup(*groupId, *result) {
  #   writeLine("serverLog", "searching users in group: *group");	
     *result = "[]";
     *resultSize = 0;
-    foreach ( *row in select order(USER_NAME), USER_ID where USER_TYPE = 'rodsuser' and USER_GROUP_ID = *groupId ) {
-        *displayName = *row.USER_NAME;
-        *userId = *row.USER_ID;
-        *userJSON = '{ "userId": "*userId", "displayName" : "*displayName" }';
-        msi_json_arrayops(*result, *userJSON, "add", *resultSize);
+    foreach ( *Row in select order(USER_NAME), USER_ID where USER_TYPE = 'rodsuser' and USER_GROUP_ID = *groupId ) {
+        *objectName = *Row.USER_NAME;
+        *objectID = *Row.USER_ID;
+        *displayName = ""
+
+        foreach( *U in select META_USER_ATTR_VALUE where USER_NAME = "*objectName" and META_USER_ATTR_NAME == "displayName" ) {
+          *displayName = *U.META_USER_ATTR_VALUE
+        }
+
+        if (*displayName == "") {
+          *displayName = *objectName
+        }
+
+        *userObject = '{ "userName" : "*objectName", "userId" : "*objectID", "displayName" : "*displayName" }';
+        msi_json_arrayops(*result, *userObject, "add", *resultSize);
     }
 }
 

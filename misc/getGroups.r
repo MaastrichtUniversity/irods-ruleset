68af1bc30ae34053b9eb8078d5bb7995e7e7cdef
jonathanMELIUS <meliusjonathan@gmail.com>
2020-09-03 14:15:38 +0200

    Change the output of the rule to an array of GroupObject [DHS-499, DHS-794]

--- misc/getGroups.r
+++ misc/getGroups.r
@@ -11,13 +11,27 @@ IRULE_getGroups(*showSpecialGroups, *result) {
     *groups = '[]';
     *groupsSize = 0;
 
-    foreach ( *Row in select USER_NAME where USER_TYPE = 'rodsgroup') {
+    foreach ( *Row in select USER_NAME, USER_ID where USER_TYPE = 'rodsgroup') {
+        *objectName = *Row.USER_NAME;
+        *objectID = *Row.USER_ID;
+        *displayName = ""
+
+        foreach( *U in select META_USER_ATTR_VALUE where USER_NAME = "*objectName" and META_USER_ATTR_NAME == "displayName" ) {
+          *displayName = *U.META_USER_ATTR_VALUE
+        }
+
+        if (*displayName == "") {
+          *displayName = *objectName
+        }
+
+        *groupObject = '{ "userName" : "*objectName", "userId" : "*objectID", "displayName" : "*displayName" }';
+
         if ( str(*showSpecialGroups) == "false" ) {
             if ( *Row.USER_NAME != "public" &&  *Row.USER_NAME != "rodsadmin" && *Row.USER_NAME != "DH-ingest" && *Row.USER_NAME != "DH-project-admins" ) {
-                msi_json_arrayops(*groups, *Row.USER_NAME, "add", *groupsSize);
+                msi_json_arrayops(*groups, *groupObject, "add", *groupsSize);
             }
          } else {
-            msi_json_arrayops(*groups, *Row.USER_NAME, "add", *groupsSize);
+            msi_json_arrayops(*groups, *groupObject, "add", *groupsSize);
          }
     }
 

68377ca7ce51515361bf96f0a217c68291334055
JonathanMELIUS <meliusjonathan@gmail.com>
2021-02-11 14:49:02 +0100

    Add new rule get_user_group_memberships [DHS-1141, DHS-1262]

---
+++ tests/test_get_user_group_memberships.r
@@ -0,0 +1,16 @@
+# Call with
+#
+# irule -r irods_rule_engine_plugin-python-instance -F test_get_user_group_memberships.r "*show_special_groups='true'" "*username='jmelius'"
+
+def main(rule_args, callback, rei):
+    show_special_groups = global_vars["*show_special_groups"][1:-1]
+    username = global_vars["*username"][1:-1]
+
+    output = callback.get_user_group_memberships(show_special_groups, username, "result")
+
+    callback.writeLine("stdout", output["arguments"][2])
+
+
+
+INPUT *show_special_groups="", *username=""
+OUTPUT ruleExecOut

40e62236a3fcdb5d20fcc498ce605841e2bc2cd0
JonathanMELIUS <meliusjonathan@gmail.com>
2021-02-18 10:18:23 +0100

    Add new rule get_managing_project [DHS-1143, DHS-1241]

---
+++ python/get_managing_project.py
@@ -0,0 +1,55 @@
+import unicodedata
+
+
+@make(inputs=[0], outputs=[1], handler=Output.STORE)
+def get_managing_project(ctx, project_id):
+    """
+    Query the list of ACL for a project for the client user
+
+    Parameters
+    ----------
+    ctx : Context
+        Combined type of a callback and rei struct.
+    project_id : str
+        The project's id; eg.g P000000010
+
+    Returns
+    -------
+    dict
+        The list of usernames for managers, contributors and viewers.
+        Returns an empty list if the user is not a manager.
+    """
+
+    # Get the client username
+    username = ''
+    var_map = session_vars.get_map(ctx.rei)
+    user_type = 'client_user'
+    userrec = var_map.get(user_type, '')
+    if userrec:
+        username = userrec.get('user_name', '')
+
+    result = ctx.callback.listProjectManagers(project_id, "managers")
+    managers = result["arguments"][1].decode('utf-8')
+    # Remove unicode 'u' from the string result
+    managers = "".join(ch for ch in managers if unicodedata.category(ch)[0] != "C")
+    managers = json.loads(managers)
+
+    # if the user is not part of the managers, return an empty list
+    if username not in managers['users']:
+        return []
+
+    result = ctx.callback.listProjectContributors(project_id, 'false', "contributors")
+    contributors = result["arguments"][2]
+    contributors = json.loads(contributors)
+
+    result = ctx.callback.listProjectViewers(project_id, 'false', "viewers")
+    viewers = result["arguments"][2]
+    viewers = json.loads(viewers)
+
+    output = {
+        "managers": managers,
+        "contributors": contributors,
+        "viewers": viewers,
+    }
+
+    return output
---
+++ tests/test_get_managing_project.r
@@ -0,0 +1,15 @@
+# Call with
+#
+# irule -r irods_rule_engine_plugin-python-instance -F test_get_managing_project.r "*project_id='P000000010'"
+
+def main(rule_args, callback, rei):
+    project_id = global_vars["*project_id"][1:-1]
+
+    output = callback.get_managing_project(project_id,  "result")
+
+    callback.writeLine("stdout", output["arguments"][1])
+
+
+
+INPUT *project_id=""
+OUTPUT ruleExecOut

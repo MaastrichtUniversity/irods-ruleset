20ce61dac212d898ee6fbfe3b89eeca4b913da81
JonathanMELIUS <meliusjonathan@gmail.com>
2021-02-19 14:10:09 +0100

    Split part of list_projects into get_project_details [DHS-1144, DHS-1239]

--- python/list_projects.py
+++ python/list_projects.py
@@ -23,53 +23,8 @@ def list_projects(ctx):
                                AS_LIST,
                                ctx.callback):
 
-        # Reset the project dictionary
-        project = {}
-
-        project["path"] = result[0]
-        project["project"] = project["path"].split("/")[3]
-
-        # List Contributors
-        ret = ctx.callback.listProjectContributors(project["project"], "false", "")["arguments"][2]
-        project["contributors"] = json.loads(ret)
-
-        # List Managers
-        ret = ctx.callback.listProjectManagers(project["project"], "")["arguments"][1]
-        project["managers"] = json.loads(ret)
-
-        # List Viewers
-        ret = ctx.callback.listProjectViewers(project["project"], "false", "")["arguments"][2]
-        project["viewers"] = json.loads(ret)
-
-        # Get project metadata
-        # Note: Retrieving the rule outcome is done with '["arguments"][2]'
-        project["title"] = ctx.callback.getCollectionAVU(project["path"], "title", "", "", "true")["arguments"][2]
-        project["enableOpenAccessExport"] = ctx.callback.getCollectionAVU(project["path"], "enableOpenAccessExport", "", "false", "false")["arguments"][2]
-        project["enableArchive"] = ctx.callback.getCollectionAVU(project["path"], "enableArchive", "", "false", "false")["arguments"][2]
-        project["respCostCenter"] = ctx.callback.getCollectionAVU(project["path"], "responsibleCostCenter", "", "", "true")["arguments"][2]
-        project["storageQuotaGiB"] = ctx.callback.getCollectionAVU(project["path"], "storageQuotaGb", "", "", "true")["arguments"][2]
-
-        # Get the display name value for PI and data steward
-        ret = ctx.callback.getCollectionAVU(project["path"], "OBI:0000103", "", "", "true")["arguments"][2]
-        for user in project["managers"]['userObjects']:
-            if user['userName'] == ret:
-                project["principalInvestigatorDisplayName"] = user['displayName']
-
-        ret = ctx.callback.getCollectionAVU(project["path"], "dataSteward", "", "", "true")["arguments"][2]
-        for user in project["managers"]['userObjects']:
-            if user['userName'] == ret:
-                project["dataStewardDisplayName"] = user['displayName']
-
-        # Calculate size for entire project
-        proj_size = float(0)
-        for proj_coll in row_iterator("COLL_NAME",
-                                      "COLL_PARENT_NAME = '" + project["path"] + "'",
-                                      AS_LIST,
-                                      ctx.callback):
-            coll_size = float(ctx.callback.get_collection_size(proj_coll[0], "GiB", "none", "")["arguments"][3])
-            proj_size = proj_size + coll_size
-
-        project["dataSizeGiB"] = proj_size
+        ret = ctx.callback.get_project_details(result[0], '')["arguments"][1]
+        project = json.loads(ret)
 
         # Append this project to the list
         output.append(project)

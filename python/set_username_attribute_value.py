088fd09289fc64765a3d164c7776f0c41f819aa8
JonathanMELIUS <meliusjonathan@gmail.com>
2021-04-15 15:45:33 +0200

    Add new rule set_username_attribute_value [DHS-1223, DHS-1420]

---
+++ python/set_username_attribute_value.py
@@ -0,0 +1,24 @@
+@make(inputs=[0, 1, 2], outputs=[], handler=Output.STORE)
+def set_username_attribute_value(ctx, username, attribute, value):
+    """
+    Set an attribute value to the input user
+
+    Parameters
+    ----------
+    ctx : Context
+        Combined type of a callback and rei struct.
+    username : str
+        The username
+    attribute : str
+        The user attribute to set
+    value : str
+        The user attribute's value to set
+
+    Returns
+    -------
+    dict
+        The attribute value
+    """
+    kvp = ctx.callback.msiString2KeyValPair('{}={}'.format(attribute, value), irods_types.BytesBuf())["arguments"][1]
+    ctx.callback.msiSetKeyValuePairsToObj(kvp, username, "-u")
+    ctx.callback.msiWriteRodsLog("INFO: {}: Setting '{}' to '{}'".format(username, attribute, value), 0)

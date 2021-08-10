cd21c19db4d929caea79fdf089cb233e04a15578
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2021-08-10 11:35:15 +0200

    Added check on drop zone path before subprocess call [DHS-1678]

--- python/set_dropzone_total_size_avu.py
+++ python/set_dropzone_total_size_avu.py
@@ -18,6 +18,15 @@ def set_dropzone_total_size_avu(ctx, token):
         The attribute value
     """
     drop_zone_path = '/nlmumc/ingest/zones/{}'.format(token)
+
+    # This call makes sure that the dropzone path exists. If it does not exist,
+    # iRODS will throw an exception and the rule execution will not continue
+    try:
+        ctx.callback.msiObjStat(drop_zone_path, irods_types.RodsObjStat())
+    except RuntimeError:
+        # -814000 CAT_UNKNOWN_COLLECTION
+        ctx.callback.msiExit("-814000", "Unknown ingest zone")
+
     list_files = subprocess.check_output(["ils", "-lr", drop_zone_path])
     lines = list_files.splitlines()[1:]
     total_size = 0

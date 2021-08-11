d932eaf944db2f3bc9407743dfb30ce64245dfaa
Luis Sago <lws.sago@gmail.com>
2021-08-11 17:51:49 +0200

    fix: rename test_set_dropzone_total_files_avu to .._total_size_avu
    DHS-1678

--- tests/test_set_dropzone_total_size_avu.r
+++ tests/test_set_dropzone_total_size_avu.r
@@ -1,11 +1,11 @@
 # Call with
 #
-# irule -r irods_rule_engine_plugin-python-instance -F test_set_dropzone_total_files_avu.r "*token='vast-chinchilla'"
+# irule -r irods_rule_engine_plugin-python-instance -F test_set_dropzone_total_size_avu.r "*token='vast-chinchilla'"
 
 def main(rule_args, callback, rei):
     token = global_vars["*token"][1:-1]
 
-    output = callback.set_dropzone_total_files_avu(token, "result")
+    output = callback.set_dropzone_total_size_avu(token, "result")
 
     callback.writeLine("stdout", output["arguments"][0])
 

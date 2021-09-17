23b59bebd94828eb3444752832f099c1f112750e
JonathanMELIUS <meliusjonathan@gmail.com>
2021-09-17 12:22:37 +0200

    Change output format from flat to nested [DHS-1769, DHS-1793]

--- python/get_collection_size_per_resource.py
+++ python/get_collection_size_per_resource.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+import re
+
 @make(inputs=[0], outputs=[1], handler=Output.STORE)
 def get_collection_size_per_resource(ctx, project):
     """
@@ -11,34 +14,46 @@ def get_collection_size_per_resource(ctx, project):
     Returns
     -------
     """
-    # set collection path based on input
-    project_path = "/nlmumc/projects/{}/%".format(project)
-    total_sizes = {}
-    for row in row_iterator("COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
-                            "META_COLL_ATTR_NAME like 'dcat:byteSize' AND COLL_NAME like '{}'".format(
-                                project_path),
-                            AS_LIST,
-                            ctx.callback):
-        total_sizes[row[0]] = row[2]
-    # create empty output list
-    output = []
-    # query size of collection per resource attribute (resource ID)
-    for row in row_iterator("COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
-                            "META_COLL_ATTR_NAME like 'dcat:byteSize_resc%' AND COLL_NAME like '{}'".format(project_path),
-                            AS_LIST,
-                            ctx.callback):
-        relative_size = (float(row[2]) / float(total_sizes[row[0]])) * 100
-        result = dict(collection=row[0], resourceAttr=row[1][19:], size=row[2], relative_size = round(relative_size, 1))
-        output.append(result)
-    # create empty values dictionary
-    values = {}
+    # create empty output dict
+    output = OrderedDict()
+
+    # create empty resources dictionary
+    resources = {}
     # query resources (names) and their respective attributes (IDs)
     for row in row_iterator("RESC_NAME, RESC_ID",
                             "",
                             AS_LIST,
                             ctx.callback):
-        values[row[1]] = row[0]
-    # append resources (names) to output based on attributes (IDs)
-    for i in range(len(output)):
-        output[i]["resource"] = values[output[i]["resourceAttr"]]
+        resources[row[1]] = row[0]
+
+    # set collection path based on input
+    project_path = "/nlmumc/projects/{}/%".format(project)
+    total_sizes = {}
+    for row in row_iterator("COLL_NAME, META_COLL_ATTR_VALUE",
+                            "META_COLL_ATTR_NAME like 'dcat:byteSize' AND COLL_NAME like '{}'".format(
+                                project_path),
+                            AS_LIST,
+                            ctx.callback):
+        collection_id = re.sub("^/nlmumc/projects/P[0-9]{9}/", "", row[0])
+        total_sizes[collection_id] = row[1]
+
+    # query size of collection per resource attribute (resource ID)
+    for row in row_iterator("COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
+                            "META_COLL_ATTR_NAME like 'dcat:byteSize_resc%' AND COLL_NAME like '{}'".format(project_path),
+                            AS_LIST,
+                            ctx.callback):
+        collection_id = re.sub("^/nlmumc/projects/P[0-9]{9}/", "", row[0])
+        resource_id = re.sub("^dcat:byteSize_resc_", "", row[1])
+        relative_size = (float(row[2]) / float(total_sizes[collection_id])) * 100
+        result = {
+                "resourceName": resources[resource_id],
+                "resourceId": resource_id,
+                "size": row[2],
+                "relativeSize": round(relative_size, 1),
+            }
+        if collection_id in output:
+            output[collection_id].append(result)
+        else:
+            output[collection_id] = [result]
+
     return output

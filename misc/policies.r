2818e7ba308eebf6bd70e1a1160155ce5851ecc3
Dean Linssen <linssen.ddj@gmail.com>
2021-11-02 11:39:07 +0100

    Added some comments on new policy [DHS-1893]

--- misc/policies.r
+++ misc/policies.r
@@ -79,20 +79,19 @@ acPreprocForCollCreate {
 }
 
 acPostProcForCollCreate {
-    ### Policy to regulate folder creation within projects ###
+    ### Policy to increment the value of the 'latest_project_number' AVU on /nlmumc/projects ###
     if ($collName like regex "/nlmumc/projects/P[0-9]{9}") {
-        *max = 0;
+        *latest = 0;
         # Find out the current max project number
         foreach ( *Row in SELECT COLL_NAME WHERE COLL_PARENT_NAME = '/nlmumc/projects' ) {
             uuChopPath(*Row.COLL_NAME, *path, *c);
-
             *i = int(substr(*c, 1, 10));
 
-            if ( *i > *max ) {
-                *max = *i;
+            if ( *i > *latest ) {
+                *latest = *i;
             }
         }
-        msiAddKeyVal(*latestProjectNumberAVU, "latest_project_number", str(*max));
+        msiAddKeyVal(*latestProjectNumberAVU, "latest_project_number", str(*latest));
         msiSetKeyValuePairsToObj(*latestProjectNumberAVU, "/nlmumc/projects", "-C");
     }
 }

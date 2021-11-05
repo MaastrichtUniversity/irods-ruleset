1498e15a81d12797358f9fc975865a0f7a43e90b
Dean Linssen <linssen.ddj@gmail.com>
2021-11-05 11:37:52 +0100

    Also now setting state in validation rule [DHS-1839, DHS-1970]

--- python/validate_metadata.py
+++ python/validate_metadata.py
@@ -1,7 +1,7 @@
 import jsonschema
 
 @make(inputs=[0], outputs=[1], handler=Output.STORE)
-def validate_metadata(ctx, token):
+def validate_metadata(ctx, source_collection):
     """
     Validate the metadata that is about to be ingested
 
@@ -9,35 +9,38 @@ def validate_metadata(ctx, token):
     ----------
     ctx : Context
         Combined type of a callback and rei struct.
-    token: str
-        The token of the dropzone to validate the metadata from
+    source_collection: str
+        The collection to validate the metadata from (ie /nlmumc/ingest/zones/bla-token)
 
     Returns
     -------
     bool
         True if valid, False if not
     """
-    instance = read_data_object_from_irods(ctx, "/nlmumc/ingest/zones/{}/instance.json".format(token))
-    schema = read_data_object_from_irods(ctx, "/nlmumc/ingest/zones/{}/schema.json".format(token))
+    instance = read_data_object_from_irods(ctx, "{}/instance.json".format(source_collection))
+    schema = read_data_object_from_irods(ctx, "{}/schema.json".format(source_collection))
 
-    is_valid = False
     try:
         instance_object = json.loads(instance)
     except ValueError:
-        ctx.callback.msiWriteRodsLog("JSONschema validation failed to parse instance.json for '{}'".format(token), 0)
+        ctx.callback.msiWriteRodsLog("JSONschema validation failed to parse instance.json for '{}'".format(source_collection), 0)
 
     try:
         schema_object = json.loads(schema)
     except ValueError:
-        ctx.callback.msiWriteRodsLog("JSONschema validation failed to parse schema.json for '{}'".format(token), 0)
+        ctx.callback.msiWriteRodsLog("JSONschema validation failed to parse schema.json for '{}'".format(source_collection), 0)
 
     try:
         # The actual validation occurs here. This can throw two types of exceptions, which we catch below
         jsonschema.validate(instance_object, schema_object)
-        is_valid = True
+        kvp = ctx.callback.msiString2KeyValPair('{}={}'.format('state', "in-queue-for-ingestion"), irods_types.BytesBuf())["arguments"][1]
+        ctx.callback.msiSetKeyValuePairsToObj(kvp, source_collection, "-C")
+        return True
     except jsonschema.exceptions.ValidationError:
-        ctx.callback.msiWriteRodsLog("JSONschema validation error occurred for '{}'".format(token), 0)
+        ctx.callback.msiWriteRodsLog("JSONschema validation error occurred for '{}'".format(source_collection), 0)
     except jsonschema.exceptions.SchemaError:
-        ctx.callback.msiWriteRodsLog("JSONschema schema error occurred for '{}'".format(token), 0)
+        ctx.callback.msiWriteRodsLog("JSONschema schema error occurred for '{}'".format(source_collection), 0)
 
-    return is_valid
+    ctx.callback.setErrorAVU(source_collection, "state", "warning-validation-incorrect", "Metadata is incorrect")
+
+    return False

4d3453f46e895edd37438b533ae7db58ac11ec1e
Dean Linssen <linssen.ddj@gmail.com>
2022-01-17 14:37:06 +0100

    Applied Black [DHS-1987, DHS-2041]

--- python/post_ingest.py
+++ python/post_ingest.py
@@ -50,7 +50,9 @@ def post_ingest(ctx, project_id, username, token, collection_id, ingest_resource
         ctx.callback.msiWriteRodsLog(
             "Retrieving multiple PID's failed for {}, leaving blank".format(destination_collection), 0
         )
-        ctx.callback.set_post_ingestion_error_avu(source_collection, "state", "error-post-ingestion", "Unable to register PID's for root","")
+        ctx.callback.set_post_ingestion_error_avu(
+            source_collection, "state", "error-post-ingestion", "Unable to register PID's for root", ""
+        )
     elif "collection" not in handle_pids or handle_pids["collection"]["handle"] == "":
         ctx.callback.msiWriteRodsLog(
             "Retrieving PID for root collection failed for {}, leaving blank".format(destination_collection), 0
@@ -79,7 +81,9 @@ def post_ingest(ctx, project_id, username, token, collection_id, ingest_resource
         # Create metadata_versions and copy schema and instance from root to that folder as version 1
         ctx.callback.create_ingest_metadata_versions(project_id, collection_id)
     except RuntimeError:
-        ctx.callback.setErrorAVU(source_collection, "state", "error-post-ingestion", "Failed to create metadata ingest snapshot")
+        ctx.callback.setErrorAVU(
+            source_collection, "state", "error-post-ingestion", "Failed to create metadata ingest snapshot"
+        )
 
     # Requesting PID's for Project Collection version 1 (includes instance and schema)
     handle_pids_version = ctx.callback.get_versioned_pids(project_id, collection_id, "1", "")["arguments"][3]
@@ -89,8 +93,9 @@ def post_ingest(ctx, project_id, username, token, collection_id, ingest_resource
         ctx.callback.msiWriteRodsLog(
             "Retrieving multiple PID's failed for {} version 1, leaving blank".format(destination_collection), 0
         )
-        ctx.callback.setErrorAVU(source_collection, "state", "error-post-ingestion",
-                                 "Unable to register PID's for version 1")
+        ctx.callback.setErrorAVU(
+            source_collection, "state", "error-post-ingestion", "Unable to register PID's for version 1"
+        )
 
     # Set latest version number to 1 for metadata latest version
     ctx.callback.setCollectionAVU(destination_collection, "latest_version_number", "1")

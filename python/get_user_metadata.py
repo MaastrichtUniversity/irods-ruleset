4267fa8291f1ce153cd7807f7d373f6595a61991
Dean Linssen <linssen.ddj@gmail.com>
2022-01-27 09:54:17 +0100

    Add max to split for people with multiple usernames

--- python/get_user_metadata.py
+++ python/get_user_metadata.py
@@ -8,7 +8,7 @@ def get_user_metadata(ctx, username):
     ret = ctx.get_username_attribute_value(result["username"], "displayName", "result")["arguments"][2]
     result["displayName"] = json.loads(ret)["value"]
 
-    split_display_name = result["displayName"].split(" ")
+    split_display_name = result["displayName"].split(" ", 1)
     result["givenName"] = split_display_name[0]
     result["familyName"] = split_display_name[1]
 

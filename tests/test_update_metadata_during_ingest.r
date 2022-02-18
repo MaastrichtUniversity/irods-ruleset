12680ef857f9e75c8dbf3946cd4a7db4dac3cb60
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-02-18 12:10:41 +0100

    Update tests for update_metadata_during_edit_collection and update_metadata_during_ingest[DHS-2139, DHS-2066]

--- tests/test_update_metadata_during_ingest.r
+++ tests/test_update_metadata_during_ingest.r
@@ -1,6 +1,6 @@
 # Call with
 #
-# irule -r irods_rule_engine_plugin-python-instance -F test_update_instance.r "*project='P000000014'" "*collection='C000000001'" "*handle='21.T12996/P000000014C000000001'" "*version='5'"
+# irule -r irods_rule_engine_plugin-python-instance -F test_update_metadata_during_ingest.r "*project='P000000014'" "*collection='C000000001'" "*handle='21.T12996/P000000014C000000001'" "*version='5'"
 
 def main(rule_args, callback, rei):
     project = global_vars["*project"][1:-1]
@@ -8,7 +8,7 @@ def main(rule_args, callback, rei):
     handle = global_vars["*handle"][1:-1]
     version = global_vars["*version"][1:-1]
 
-    output = callback.update_instance(project, collection, handle, version)
+    output = callback.update_metadata_during_ingest(project, collection, handle, version)
 
     callback.writeLine("stdout", "update_instance Done")
 

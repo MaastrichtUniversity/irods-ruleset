6ec6006c0f7d1938fcd743a38bda4d653d0a8c65
JonathanMELIUS <meliusjonathan@gmail.com>
2022-03-28 16:08:49 +0200

    Add new rule get_user_internal_affiliation_status [DHS-1846, DHS-2194]

---
+++ python/users/get_user_internal_affiliation_status.py
@@ -0,0 +1,23 @@
+@make(inputs=[0], outputs=[1], handler=Output.STORE)
+def get_user_internal_affiliation_status(ctx, username):
+    """
+    Get the user voPersonExternalID and check if the user is part of the UM or MUMC organization.
+
+    Parameters
+    ----------
+    ctx
+    username: str
+        The user to check
+
+    Returns
+    -------
+    bool
+        True, if the user is from the UM or MUMC organization. Otherwise, False.
+    """
+    ret = ctx.get_user_attribute_value(username, "voPersonExternalID", "true", "result")["arguments"][3]
+    external_id = json.loads(ret)["value"]
+    affiliation = external_id.split("@")[1]
+    ctx.writeLine("stdout", affiliation)
+    if affiliation in ["maastrichtuniversity.nl", "mumc.nl"]:
+        return True
+    return False

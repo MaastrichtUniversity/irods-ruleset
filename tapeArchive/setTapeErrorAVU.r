2194b667fffa1ead976631b69a2010933252a3bb
Daniel T <danieltheunissen@users.noreply.github.com>
2022-06-01 09:39:03 +0200

    Fixed Typo

--- tapeArchive/setTapeErrorAVU.r
+++ tapeArchive/setTapeErrorAVU.r
@@ -1,7 +1,7 @@
 #irule -F setTapeErrorAVU.r "*projectCollection='/nlmumc/projects/P000000001/C000000001'" "*attribute='attr'" "*value='val'" "*message='crash'"
 
 irule_dummy() {
-    IRULE_setErrorAVU(*projectCollection, *attribute, *value,*message);
+    IRULE_setTapeErrorAVU(*projectCollection, *attribute, *value,*message);
 }
 
 

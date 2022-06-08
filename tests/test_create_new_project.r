f052b331f8c4275fe85a70aa46488b817d154d59
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-06-08 16:46:31 +0200

    Update test of create_new_project to include shared dropzones config [DHS-2345, DHS-2160]

--- tests/test_create_new_project.r
+++ tests/test_create_new_project.r
@@ -1,6 +1,6 @@
 # Call with
 #
-# irule -r irods_rule_engine_plugin-python-instance -F /rules/tests/test_create_new_project.r "*authorizationPeriodEndDate='1-1-2018'" "*dataRetentionPeriodEndDate='1-1-2018'" "*ingestResource='iresResource'" "*resource='replRescUM01'" "*storageQuotaGb='10'" "*title='Testing'" "*principalInvestigator='p.rofessor@maastrichtuniversity.nl'" "*dataSteward='d.steward@maastrichtuniversity.nl'" "*respCostCenter='UM-30001234X'" "*openAccess='false'" "*tapeArchive='true'" "*tapeUnarchive='true'" "*collectionMetadataSchemas='DataHub_general_schema'"
+# irule -r irods_rule_engine_plugin-python-instance -F /rules/tests/test_create_new_project.r "*authorizationPeriodEndDate='1-1-2018'" "*dataRetentionPeriodEndDate='1-1-2018'" "*ingestResource='iresResource'" "*resource='replRescUM01'" "*storageQuotaGb='10'" "*title='Testing'" "*principalInvestigator='p.rofessor@maastrichtuniversity.nl'" "*dataSteward='d.steward@maastrichtuniversity.nl'" "*respCostCenter='UM-30001234X'" "*openAccess='false'" "*tapeArchive='true'" "*tapeUnarchive='true'" "*sharedDropzones='true'" "*collectionMetadataSchemas='DataHub_general_schema'"
 
 def main(rule_args, callback, rei):
     authorizationPeriodEndDate = global_vars["*authorizationPeriodEndDate"][1:-1]
@@ -15,6 +15,7 @@ def main(rule_args, callback, rei):
     openAccess = global_vars["*openAccess"][1:-1]
     tapeArchive = global_vars["*tapeArchive"][1:-1]
     tapeUnarchive = global_vars["*tapeUnarchive"][1:-1]
+    sharedDropzones = global_vars["*sharedDropzones"][1:-1]
     collectionMetadataSchemas = global_vars["*collectionMetadataSchemas"][1:-1]
 
 
@@ -23,12 +24,12 @@ def main(rule_args, callback, rei):
                                                    principalInvestigator, dataSteward,
                                                    respCostCenter, openAccess,
                                                    tapeArchive, tapeUnarchive,
-                                                    collectionMetadataSchemas,
+                                                   sharedDropzones, collectionMetadataSchemas,
                                                    "result")
 
-    callback.writeLine("stdout", output["arguments"][13])
+    callback.writeLine("stdout", output["arguments"][14])
 
 
 
-INPUT *authorizationPeriodEndDate="", *dataRetentionPeriodEndDate="", *ingestResource="", *resource="", *storageQuotaGb="", *title="", *principalInvestigator="", *dataSteward="", *respCostCenter="", *openAccess="", *tapeArchive="", *tapeUnarchive="", *collectionMetadataSchemas=""
+INPUT *authorizationPeriodEndDate="", *dataRetentionPeriodEndDate="", *ingestResource="", *resource="", *storageQuotaGb="", *title="", *principalInvestigator="", *dataSteward="", *respCostCenter="", *openAccess="", *tapeArchive="", *tapeUnarchive="", *sharedDropzones="", *collectionMetadataSchemas=""
 OUTPUT ruleExecOut

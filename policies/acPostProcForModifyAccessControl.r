d1fcf3f5e9f783e5d489148b71f464f83e46c58f
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2022-06-09 14:33:41 +0200

    Added a comment to new policy [DHS-2160]

--- policies/acPostProcForModifyAccessControl.r
+++ policies/acPostProcForModifyAccessControl.r
@@ -1,3 +1,4 @@
+# This policy is triggered after ACLs have been modified:
 acPostProcForModifyAccessControl(*RecursiveFlag,*AccessLevel,*UserName,*Zone,*Path) {
     if(*AccessLevel != "read" && *Path like regex "/nlmumc/projects/P[0-9]{9}") {
         uuChop(*Path, *head, *projectId, "/nlmumc/projects/", true);

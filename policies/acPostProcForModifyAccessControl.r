14727e8c68f064ae41e6a34a0423bc1ee90aa808
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2022-06-10 14:34:55 +0200

    Remove extra condition in policy [DHS-2160]

--- policies/acPostProcForModifyAccessControl.r
+++ policies/acPostProcForModifyAccessControl.r
@@ -1,6 +1,6 @@
 # This policy is triggered after ACLs have been modified:
 acPostProcForModifyAccessControl(*RecursiveFlag,*AccessLevel,*UserName,*Zone,*Path) {
-    if(*AccessLevel != "read" && *Path like regex "/nlmumc/projects/P[0-9]{9}") {
+    if(*Path like regex "/nlmumc/projects/P[0-9]{9}") {
         uuChop(*Path, *head, *projectId, "/nlmumc/projects/", true);
         transfer_project_acl_to_dropzone_single(*projectId, *UserName)
     }

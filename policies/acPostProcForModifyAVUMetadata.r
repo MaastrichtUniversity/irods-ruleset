ac331db09c267f7f583feacbac5399dcafd62df3
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2022-06-16 14:49:30 +0200

    Added comments to policies [DHS-2160]

--- policies/acPostProcForModifyAVUMetadata.r
+++ policies/acPostProcForModifyAVUMetadata.r
@@ -2,6 +2,8 @@
 # imeta add, adda, addw, set, rm, rmw, rmi
 acPostProcForModifyAVUMetadata(*Option,*ItemType,*ItemName,*AName,*AValue,*AUnit) {
 #    msiWriteRodsLog("DEBUG: ADD, SET, RM option kicked off", *status);
+
+    # This policy modifies all dropzone ACLs for the project ID that was modified
     if(*AName == "enableDropzoneSharing" && *ItemName like regex "/nlmumc/projects/P[0-9]{9}") {
          uuChop(*ItemName, *head, *projectId, "/nlmumc/projects/", true);
          set_project_acl_to_dropzones(*projectId)
--- policies/acPostProcForModifyAccessControl.r
+++ policies/acPostProcForModifyAccessControl.r
@@ -1,5 +1,6 @@
 # This policy is triggered after ACLs have been modified:
 acPostProcForModifyAccessControl(*RecursiveFlag,*AccessLevel,*UserName,*Zone,*Path) {
+    # This policy modifies all dropzone ACLs for the project ID that was modified
     if(*Path like regex "/nlmumc/projects/P[0-9]{9}") {
         uuChop(*Path, *head, *projectId, "/nlmumc/projects/", true);
         *eventMessage = "*Path: User $userNameClient sets '*UserName' to '*AccessLevel'"

2c444f5dd4f67a880747d9a819a10d6c5608dc66
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-06-21 14:56:07 +0200

    Update list_collections.py to include the user uploaded file count [DHS-2360, DHS-2266]

--- python/collections/list_collections.py
+++ python/collections/list_collections.py
@@ -1,3 +1,4 @@
+# /rules/tests/run_test.sh -r list_collections -a "/nlmumc/projects/P000000014" -j
 @make(inputs=[0], outputs=[1], handler=Output.STORE)
 def list_collections(ctx, project_path):
     """
@@ -15,6 +16,11 @@ def list_collections(ctx, project_path):
     list
         a json list of collections objects
     """
+    # Files to exclude from the user uploaded files
+    num_files_exclusion = ["metadata.xml"]
+
+    project_id = formatters.get_project_id_from_project_path(project_path)
+
     # Initialize the collections dictionary
     project_collections = []
 
@@ -31,12 +37,41 @@ def list_collections(ctx, project_path):
 
         # Get AVUs
         project_collection["size"] = coll_size
-        project_collection["title"] = ctx.callback.getCollectionAVU(proj_coll[0], "title", "", "", FALSE_AS_STRING)["arguments"][2]
-        project_collection["creator"] = ctx.callback.getCollectionAVU(proj_coll[0], "creator", "", "", FALSE_AS_STRING)["arguments"][2]
-        project_collection["PID"] = ctx.callback.getCollectionAVU(proj_coll[0], "PID", "", "", FALSE_AS_STRING)["arguments"][2]
-        project_collection["numFiles"] = ctx.callback.getCollectionAVU(proj_coll[0], "numFiles", "", "", FALSE_AS_STRING)["arguments"][
-            2
-        ]
+        project_collection["title"] = ctx.callback.getCollectionAVU(proj_coll[0], "title", "", "", FALSE_AS_STRING)[
+            "arguments"
+        ][2]
+        project_collection["creator"] = ctx.callback.getCollectionAVU(proj_coll[0], "creator", "", "", FALSE_AS_STRING)[
+            "arguments"
+        ][2]
+        project_collection["PID"] = ctx.callback.getCollectionAVU(proj_coll[0], "PID", "", "", FALSE_AS_STRING)[
+            "arguments"
+        ][2]
+        project_collection["numFiles"] = ctx.callback.getCollectionAVU(
+            proj_coll[0], "numFiles", "", "", FALSE_AS_STRING
+        )["arguments"][2]
+
+        # Calculate the number of user uploaded files
+        system_files = 0
+
+        latest_version_number = ctx.callback.getCollectionAVU(
+            proj_coll[0], "latest_version_number", "", "", FALSE_AS_STRING
+        )["arguments"][2]
+        metadata_files = (int(latest_version_number) * 2) + 2
+        system_files = system_files + metadata_files
+
+        project_collection_path = formatters.format_project_collection_path(project_id, project_collection["id"])
+        for file in num_files_exclusion:
+            # Check if the file exist
+            for file in row_iterator(
+                "DATA_NAME",
+                "COLL_NAME = '" + project_collection_path + "' AND DATA_NAME = '" + file + "'",
+                AS_LIST,
+                ctx.callback,
+            ):
+                if file:
+                    system_files = system_files + 1
+
+        project_collection["numUserFiles"] = str(int(project_collection["numFiles"]) - system_files)
 
         project_collections.append(project_collection)
 

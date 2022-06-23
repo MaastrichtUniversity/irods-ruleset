cfb9829bdad149d551f9a9f75831f746e597ceea
JonathanMELIUS <meliusjonathan@gmail.com>
2022-06-23 10:42:27 +0200

    Refactor num_files_exclusion loop & Cast numUserFiles & numFiles as int [DHS-2266, DHS-2360]

--- python/collections/list_collections.py
+++ python/collections/list_collections.py
@@ -17,7 +17,8 @@ def list_collections(ctx, project_path):
         a json list of collections objects
     """
     # Files to exclude from the user uploaded files
-    num_files_exclusion = ["metadata.xml"]
+    # How to extend the list: "'metadata.xml', 'foobar.json'"
+    num_files_exclusion = "'metadata.xml'"
 
     project_id = formatters.get_project_id_from_project_path(project_path)
 
@@ -46,12 +47,12 @@ def list_collections(ctx, project_path):
         project_collection["PID"] = ctx.callback.getCollectionAVU(proj_coll[0], "PID", "", "", FALSE_AS_STRING)[
             "arguments"
         ][2]
-        project_collection["numFiles"] = ctx.callback.getCollectionAVU(
+        project_collection["numFiles"] = int(ctx.callback.getCollectionAVU(
             proj_coll[0], "numFiles", "", "", FALSE_AS_STRING
-        )["arguments"][2]
+        )["arguments"][2])
 
         # Calculate the number of user uploaded files
-        system_files = 0
+        metadata_files = 0
 
         latest_version_number = ctx.callback.getCollectionAVU(
             proj_coll[0], "latest_version_number", "", "", FALSE_AS_STRING
@@ -59,21 +60,19 @@ def list_collections(ctx, project_path):
 
         if latest_version_number:
             metadata_files = (int(latest_version_number) * 2) + 2
-            system_files = system_files + metadata_files
 
         project_collection_path = formatters.format_project_collection_path(project_id, project_collection["id"])
-        for file in num_files_exclusion:
-            # Check if the file exist
-            for file in row_iterator(
-                "DATA_NAME",
-                "COLL_NAME = '" + project_collection_path + "' AND DATA_NAME = '" + file + "'",
-                AS_LIST,
-                ctx.callback,
-            ):
-                if file:
-                    system_files = system_files + 1
 
-        project_collection["numUserFiles"] = str(int(project_collection["numFiles"]) - system_files)
+        for row in row_iterator(
+            "DATA_NAME",
+            "COLL_NAME = '{}' AND DATA_NAME in ({})".format(project_collection_path, num_files_exclusion),
+            AS_LIST,
+            ctx.callback,
+        ):
+            if row:
+                metadata_files = metadata_files + 1
+
+        project_collection["numUserFiles"] = int(project_collection["numFiles"]) - metadata_files
 
         project_collections.append(project_collection)
 

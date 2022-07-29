381a17edf382ec6e82706328b37535007aefe4bc
JonathanMELIUS <meliusjonathan@gmail.com>
2022-07-29 14:21:49 +0200

    Add set totalSize in run_put_ingest_job [DHS-2438, DHS-2488]

--- python/ingest/run_put_ingest_job.py
+++ python/ingest/run_put_ingest_job.py
@@ -3,26 +3,23 @@
 @make(inputs=[0, 1], outputs=[2], handler=Output.STORE)
 def run_put_ingest_job(ctx, token, target_collection):
     from subprocess import check_call, CalledProcessError
+    import os
 
     source_directory = '/mnt/ingest/zones/{}'.format(token)
 
-    # import os
-    #
-    # source_directory = '/mnt/ingest/zones/{}'.format(token)
-    # total = 0
-    # start_path = source_directory
-    # for root, dirs, files in os.walk(source_directory):
-    #     total += len(files)
-    #
-    # total_size = 0
-    # for dirpath, dirnames, filenames in os.walk(start_path):
-    #     for f in filenames:
-    #         fp = os.path.join(dirpath, f)
-    #         # skip if it is symbolic link
-    #         if not os.path.islink(fp):
-    #             total_size += os.path.getsize(fp)
-    #
-    # TODO set some total AVU on the dropzone collection
+    dropzone_path = format_dropzone_path(ctx, token, "mounted")
+
+    total_size = 0
+    for dir_path, dir_names, filenames in os.walk(source_directory):
+        for f in filenames:
+            fp = os.path.join(dir_path, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    ctx.callback.msiWriteRodsLog("INFO: run_put_ingest_job: totalSize '{}'".format(total_size), 0)
+
+    ctx.callback.setCollectionAVU(dropzone_path, "totalSize", str(total_size))
 
     event_handler = "/opt/irods/event_handler_replRescUM01_put.py"
     return_code = None

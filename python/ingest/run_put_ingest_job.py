c2d40fcebe598eaa346396827f2cf3d266de930c
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-08-03 15:17:26 +0200

    Use request to irods-ingest-worker flask app in mounted ingest [DHS-2495, DHS-2464]

--- python/ingest/run_put_ingest_job.py
+++ python/ingest/run_put_ingest_job.py
@@ -2,8 +2,8 @@
 
 @make(inputs=[0, 1], outputs=[2], handler=Output.STORE)
 def run_put_ingest_job(ctx, token, target_collection):
-    from subprocess import check_call, CalledProcessError
     import os
+    import requests
 
     source_directory = '/mnt/ingest/zones/{}'.format(token)
 
@@ -21,13 +21,22 @@ def run_put_ingest_job(ctx, token, target_collection):
 
     ctx.callback.setCollectionAVU(dropzone_path, "totalSize", str(total_size))
 
-    event_handler = "/opt/irods/event_handler_put_operation.py"
-    return_code = None
+    url = 'http://irods-ingest-worker.dh.local:5000/job'
+    data = {"source": source_directory, "target": target_collection}
     try:
-        return_code = check_call(
-            ["/opt/irods/run_put_ingest_job.sh", source_directory, target_collection, event_handler]
-        )
-    except CalledProcessError as err:
-        ctx.callback.msiExit("-1", "ERROR: run_put_ingest_job: cmd '{}' retcode'{}'".format(err.cmd, err.returncode))
+        r = requests.post(url, json=data)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        ctx.callback.msiWriteRodsLog("ERROR: run_put_ingest_job: request '{}'".format(errh), 0)
+        ctx.callback.msiExit("-1", "ERROR: run_put_ingest_job: cmd '{}' retcode'{}'".format(data, errh))
+    except requests.exceptions.ConnectionError as errc:
+        ctx.callback.msiWriteRodsLog("ERROR: run_put_ingest_job: request '{}'".format(errc), 0)
+        ctx.callback.msiExit("-1", "ERROR: run_put_ingest_job: cmd '{}' retcode'{}'".format(data, errc))
+    except requests.exceptions.Timeout as errt:
+        ctx.callback.msiWriteRodsLog("ERROR: run_put_ingest_job: request '{}'".format(errt), 0)
+        ctx.callback.msiExit("-1", "ERROR: run_put_ingest_job: cmd '{}' retcode'{}'".format(data, errt))
+    except requests.exceptions.RequestException as err:
+        ctx.callback.msiWriteRodsLog("ERROR: run_put_ingest_job: request '{}'".format(err), 0)
+        ctx.callback.msiExit("-1", "ERROR: run_put_ingest_job: cmd '{}' retcode'{}'".format(data, err))
 
-    ctx.callback.msiWriteRodsLog("INFO: run_put_ingest_job: return_code '{}'".format(return_code), 0)
+    ctx.callback.msiWriteRodsLog("INFO: run_put_ingest_job: return_code '{}'".format(r.status_code), 0)

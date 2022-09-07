01575384fbb009d204ba55d0063c80ccc2ebd9a9
jonathanMELIUS <meliusjonathan@gmail.com>
2022-09-07 15:09:58 +0200

    Make get_dropzone_folders recursive [DHS-1766, DHS-2510]

--- python/drop_zones/get_dropzone_folders.py
+++ python/drop_zones/get_dropzone_folders.py
@@ -1,47 +1,91 @@
 # /rules/tests/run_test.sh -r get_dropzone_folders -a "vast-dove,/"
-@make(inputs=[0,1], outputs=[2], handler=Output.STORE)
-def get_dropzone_folders(ctx, token, path ):
+@make(inputs=[0, 1], outputs=[2], handler=Output.STORE)
+def get_dropzone_folders(ctx, token, path):
     """
-    Lists the folders and files attributes at the input 'path'
+    Lists recursively the folders and files attributes at the input 'path'
 
     Parameters
     ----------
-    ctx : Context
+    ctx: Context
         Combined type of callback and rei struct.
-    token : str
+    token: str
        The dropzone token
-    path : str
+    path: str
        Relative path in dropzone for folder
 
     Returns
     -------
     dict
-       The folders and files attributes at the requested path
+       The recursive folders list at the requested path
     """
+    absolute_path = "/nlmumc/ingest/direct/{}{}".format(token, path)
 
-    import time
+    root = []
+    walk_dropzone_folders(ctx, root, absolute_path, absolute_path)
 
-    output = {"folders": []}
+    return root
 
-    absolute_path = "/nlmumc/ingest/direct/{}{}".format(token,path)
-    # Get sub-folders
-    for result in row_iterator("COLL_NAME, COLL_CREATE_TIME, COLL_MODIFY_TIME", "COLL_PARENT_NAME LIKE '{}'".format(absolute_path), AS_LIST, ctx.callback):
 
-        # Extract only the name of the subfolder from the full name/path
+def get_collection_sub_folders(ctx, parent, parent_path, dropzone_root):
+    """
+    Query the list of sub folders at the parent location.
+
+    Parameters
+    ----------
+    ctx: Context
+        Combined type of callback and rei struct.
+    parent: dict|list
+        If the parent is the root, the input type is the output list. Otherwise, the input parent is a JSON object/dict
+    parent_path: str
+        The absolute path to query the list of sub-folders. e.g: /nlmumc/ingest/direct/vast-dove/foo/bar
+    dropzone_root: str
+        The absolute dropzone root path. e.g: /nlmumc/ingest/direct/vast-dove
+
+    Returns
+    -------
+    dict|list
+        If the parent is the root, return the updated list with the sub-folders on the root level.
+        Otherwise, return the updated parent dict with the sub-folders list on the current location in the key "data".
+    """
+    for result in row_iterator("COLL_NAME, COLL_CREATE_TIME, COLL_MODIFY_TIME",
+                               "COLL_PARENT_NAME = '{}'".format(parent_path), AS_LIST, ctx.callback):
+        # Extract only the name of the sub_folder from the full name/path
         name = result[0].rsplit('/', 1)[1]
-        relative_collection_path = result[0].replace("/nlmumc/ingest/direct/{}{}".format(token,path), "")
-
+        relative_collection_path = result[0].replace(dropzone_root, "")
         folder_node = {
-            "name": name,
+            "value": name,
             "full_path": result[0],
-            "path": relative_collection_path,
-            "type": "folder",
-            "size": "--",
-            "rescname": "--",
-            "ctime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(result[1]))),
-            "mtime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(result[2])))
+            "id": relative_collection_path,
+            "data": []
         }
+        if parent_path == dropzone_root:
+            parent.append(folder_node)
+        else:
+            parent["data"].append(folder_node)
 
-        output["folders"].append(folder_node)
+    return parent
 
-    return output
+
+def walk_dropzone_folders(ctx, parent, parent_path, dropzone_root):
+    """
+    Recursive function to walk through the entire dropzone collection and update the original input parent list.
+
+    Parameters
+    ----------
+    ctx: Context
+        Combined type of callback and rei struct.
+    parent: dict|list
+        If the parent is the root, the input type is the output list. Otherwise, the input parent is a JSON object/dict
+    parent_path: str
+        The absolute path to query the list of sub-folders. e.g: /nlmumc/ingest/direct/vast-dove/foo/bar
+    dropzone_root: str
+        The absolute dropzone root path. e.g: /nlmumc/ingest/direct/vast-dove
+    """
+    child_collections = get_collection_sub_folders(ctx, parent, parent_path, dropzone_root)
+
+    collections_list = child_collections
+    if parent_path != dropzone_root:
+        collections_list = child_collections["data"]
+
+    for child_collection in collections_list:
+        walk_dropzone_folders(ctx, child_collection, child_collection["full_path"], dropzone_root)

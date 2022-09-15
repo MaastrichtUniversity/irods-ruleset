017ae8ef88d63f11e3f406c0dbce9f3b8502f1f4
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2022-09-15 16:03:39 +0200

    Add nosec [DHS-2427, DHS-2554]

--- python/ingest/perform_irsync.py
+++ python/ingest/perform_irsync.py
@@ -1,5 +1,5 @@
 @make(inputs=range(3), outputs=[], handler=Output.STORE)
 def perform_irsync(ctx, source_collection, destination_collection, destination_resource):
-    import subprocess
-    return_code = subprocess.check_call(["irsync", "-R", destination_resource, "-r", source_collection, "i:" + destination_collection], shell=False)
+    import subprocess # nosec
+    return_code = subprocess.check_call(["irsync", "-R", destination_resource, "-r", source_collection, "i:" + destination_collection], shell=False) #nosec
     ctx.callback.msiWriteRodsLog("Exit code of irsync: " + str(return_code) ,0)

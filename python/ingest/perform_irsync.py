c5ebfae9d90592ca441d4aac7f8bd2ab712db5b4
jonathanMELIUS <meliusjonathan@gmail.com>
2022-10-13 14:51:48 +0200

    Update logs messages and add docstring [DHS-2434]

--- python/ingest/perform_irsync.py
+++ python/ingest/perform_irsync.py
@@ -1,5 +1,25 @@
+# /rules/tests/run_test.sh -r perform_irsync -a "handsome-snake,/nlmumc/projects/P000000019/C000000001,replRescUM01" -u "dlinssen"
 @make(inputs=range(3), outputs=[], handler=Output.STORE)
 def perform_irsync(ctx, token, destination_collection, destination_resource):
+    """
+    This rule is part the mounted ingest workflow.
+
+    In case of failed ingest and an admin want to restart the rule:
+        * It MUST be done on the ingestion resource server (iRES-UM or iRES-AZM), NOT iCAT.
+            * The rule needs physical access to the source collection to perform the 'irsync' call.
+        * When the rule have been successfully executed, the rule 'finish_ingest' still MUST be executed.
+
+    Parameters
+    ----------
+    ctx : Context
+        Combined type of callback and rei struct.
+    token: str
+        The dropzone token, to locate the source collection; e.g: 'handsome-snake'
+    destination_collection: str
+        The absolute path to the newly created project collection; e.g: '/nlmumc/projects/P000000018/C000000001'
+    destination_resource: str
+        The resource where the data objects will be replicated; e.g: 'replRescUM01'
+    """
     from subprocess import CalledProcessError, check_call  # nosec
     import time
 
@@ -26,7 +46,7 @@ def perform_irsync(ctx, token, destination_collection, destination_resource):
 
         if return_code != 0:
             retry_counter -= 1
-            ctx.callback.msiWriteRodsLog("DEBUG: Decrement retry_counter: {}".format(str(retry_counter)), 0)
+            ctx.callback.msiWriteRodsLog("DEBUG: Decrement irsync retry_counter: {}".format(str(retry_counter)), 0)
             time.sleep(RETRY_SLEEP_NUMBER)
         else:
             retry_counter = 0
@@ -34,6 +54,12 @@ def perform_irsync(ctx, token, destination_collection, destination_resource):
 
     if return_code != 0:
         ctx.callback.setErrorAVU(
-            dropzone_path, "state", DropzoneState.ERROR_INGESTION.value, "Error copying ingest zone"
+            dropzone_path,
+            "state",
+            DropzoneState.ERROR_INGESTION.value,
+            "Error while performing perform_irsync towards '{}:{}'".format(
+                destination_collection,
+                destination_resource
+            )
         )
 

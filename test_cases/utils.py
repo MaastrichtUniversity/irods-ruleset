382d1c3a4cb9c2e1c44ecc3a834fb104ec20ff8e
jonathanMELIUS <meliusjonathan@gmail.com>
2022-10-19 11:08:49 +0200

    Refactor add_metadata_files_to_dropzone [DHS-2595]

--- test_cases/utils.py
+++ test_cases/utils.py
@@ -33,26 +33,24 @@ def get_schema():
         json_file.write(response.content)
 
 
-def add_metadata_files_to_mounted_dropzone(token):
+def add_metadata_files_to_dropzone(token, dropzone_type):
     get_instance()
-    copy_instance = "cp {} /mnt/ingest/zones/{}/instance.json".format(TMP_INSTANCE_PATH, token)
-    subprocess.check_call(copy_instance, shell=True)
-
-    get_schema()
-    copy_schema = "cp {} /mnt/ingest/zones/{}/schema.json".format(TMP_SCHEMA_PATH, token)
-    subprocess.check_call(copy_schema, shell=True)
-
-
-def add_metadata_files_to_direct_dropzone(token):
-    get_instance()
-    instance_path = formatters.format_instance_dropzone_path(token, "direct")
+    instance_path = formatters.format_instance_dropzone_path(token, dropzone_type)
     iput_instance = "iput -R stagingResc01 {} {}".format(TMP_INSTANCE_PATH, instance_path)
     subprocess.check_call(iput_instance, shell=True)
 
     get_schema()
-    schema_path = formatters.format_schema_dropzone_path(token, "direct")
+    schema_path = formatters.format_schema_dropzone_path(token, dropzone_type)
     iput_schema = "iput -R stagingResc01 {} {}".format(TMP_SCHEMA_PATH, schema_path)
     subprocess.check_call(iput_schema, shell=True)
+
+
+def add_metadata_files_to_mounted_dropzone(token):
+    add_metadata_files_to_dropzone(token, "mounted")
+
+
+def add_metadata_files_to_direct_dropzone(token):
+    add_metadata_files_to_dropzone(token, "direct")
 
 
 def revert_latest_project_number():

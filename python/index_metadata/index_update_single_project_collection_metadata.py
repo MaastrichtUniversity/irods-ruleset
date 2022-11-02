d48a5c33264ea446795bf68c7e59de9b3451c2ea
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-11-02 15:08:33 +0100

    Add metadata indexing to changeProjectPermissions.r [DHS-2683, DHS-2586]

--- python/index_metadata/index_update_single_project_collection_metadata.py
+++ python/index_metadata/index_update_single_project_collection_metadata.py
@@ -1,12 +1,12 @@
-# /rules/tests/run_test.sh -r index_update_single_project_collection_metadata -a "P000000014,C000000001" -u service-disqover
+# /rules/tests/run_test.sh -r index_update_single_project_collection_metadata -a "P000000014,C000000001," -u service-disqover
 
-@make(inputs=[0, 1, 2], outputs=[3], handler=Output.STORE)
-def index_update_single_project_collection_metadata(ctx, project_id, collection_id, es=None):
+@make(inputs=[0, 1], outputs=[2], handler=Output.STORE)
+def index_update_single_project_collection_metadata(ctx, project_id, collection_id):
     """
     Use this rule to update a single project collection metadata (instance.json & AVUs) in the
     $COLLECTION_METADATA_INDEX.
        - connect to the $ELASTIC_HOST if no connection is provided
-       - delete existing index
+       - delete existing document
        - create a new document in the index
 
     Parameters
@@ -28,16 +28,16 @@ def index_update_single_project_collection_metadata(ctx, project_id, collection_
     """
     from elasticsearch import ElasticsearchException
 
-    if not es:
-        es = get_elastic_search_connection(ctx)
+    es = get_elastic_search_connection(ctx)
 
     try:
-        es.delete(index=COLLECTION_METADATA_INDEX, id=project_id + "_" + collection_id)
+        es.delete(index=COLLECTION_METADATA_INDEX, id=project_id + "_" + collection_id, ignore=[400, 404])
     except ElasticsearchException:
         message = "Unable to delete existing document"
         ctx.callback.writeLine("stdout", "ERROR: {}".format(message))
         ctx.callback.msiWriteRodsLog("ERROR: {}".format(message), 0)
         ctx.callback.msiWriteRodsLog("ERROR: ElasticsearchException raised during document deletion", 0)
+        return False
 
     project_collection_path = formatters.format_project_collection_path(project_id, collection_id)
     return index_project_collection(ctx, es, project_collection_path)
---
+++ python/index_metadata/index_update_single_project_metadata.py
@@ -0,0 +1,25 @@
+# /rules/tests/run_test.sh -r index_update_single_project_metadata -a "P000000014" -u service-disqover
+
+@make(inputs=[0], outputs=[], handler=Output.STORE)
+def index_update_single_project_metadata(ctx, project_id):
+    """
+       Use this rule to update all collection metadata (instance.json & AVUs) in the
+       $COLLECTION_METADATA_INDEX that belong to a specific project
+          - connect to the $ELASTIC_HOST
+          - list collections for the project
+          - for each collection
+              - update document in the index
+       Parameters
+       ----------
+       ctx : Context
+          Combined type of callback and rei struct.
+       project_id: str
+           The project ID ie P000000001
+       """
+
+    project_path = formatters.format_project_path(project_id)
+    collections = json.loads(ctx.callback.list_collections(project_path, "")["arguments"][1])
+
+    for collection in collections:
+        collection_id = collection["id"]
+        ctx.callback.index_update_single_project_collection_metadata(project_id, collection_id, "")

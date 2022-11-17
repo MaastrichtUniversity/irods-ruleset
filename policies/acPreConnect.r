27a0e3a5e1509acd318a0819f9086cb51d090304
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-11-17 14:28:56 +0100

    Update policy to require SSL again [DHS-2749, DHS-2432]

--- policies/acPreConnect.r
+++ policies/acPreConnect.r
@@ -3,4 +3,4 @@
 # "CS_NEG_REQUIRE" to demand SSL,
 # "CS_NEG_DONT_CARE" to allow the server to decide
 # Source: https://github.com/irods-contrib/metalnx-web/blob/master/docs/Getting-Started.md#setup-irods-negotiation
-acPreConnect(*OUT) { *OUT="CS_NEG_DONT_CARE"; }
+acPreConnect(*OUT) { *OUT="CS_NEG_REQUIRE"; }

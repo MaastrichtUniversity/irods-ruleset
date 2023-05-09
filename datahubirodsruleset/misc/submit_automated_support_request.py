3a03c70e896db23c2ac8eff5ae6da51135d2d382
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-05-09 13:17:38 +0200

    Move timestamp to submit_automated_support_request [DHS-3071, DHS-3053]

--- datahubirodsruleset/misc/submit_automated_support_request.py
+++ datahubirodsruleset/misc/submit_automated_support_request.py
@@ -5,25 +5,26 @@ from datahubirodsruleset.decorator import make, Output
 
 @make(inputs=[0, 1, 2, 3], outputs=[], handler=Output.STORE)
 def submit_ingest_error_automated_support_request(ctx, email, project_id, token, error_message):
-    from datetime import datetime
 
-    error_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     description = (
         "Ingest for dropzone {} (Project {}) has failed, we will contact you when we have more information "
         "available".format(token, project_id)
     )
-    ctx.callback.submit_automated_support_request(email, description, error_timestamp, error_message)
+    ctx.callback.submit_automated_support_request(email, description, error_message)
 
 
-@make(inputs=[0, 1, 2, 3], outputs=[], handler=Output.STORE)
-def submit_automated_support_request(ctx, email, description, error_timestamp, error_message):
+@make(inputs=[0, 1, 2], outputs=[], handler=Output.STORE)
+def submit_automated_support_request(ctx, email, description, error_message):
     import requests
+    from datetime import datetime
 
     # Get the Help Center Backend url
     help_center_backend_base = ctx.callback.get_env("HC_BACKEND_URL", "true", "")["arguments"][2]
-    help_center_request_endpoint = "http://{}/help_backend/submit_request/automated_process_support".format(
+    help_center_request_endpoint = "{}/help_backend/submit_request/automated_process_support".format(
         help_center_backend_base
     )
+
+    error_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     request_json = {
         "email": email,
@@ -37,8 +38,12 @@ def submit_automated_support_request(ctx, email, description, error_timestamp, e
             json=request_json,
         )
         if response.ok:
-            ctx.callback.msiWriteRodsLog("SUCCESS: TICKET Created '{}'".format(response.status_code), 0)
+            issue_key = response.json()["issueKey"]
+            ctx.callback.msiWriteRodsLog("Support ticket '{}' created after process error".format(issue_key), 0)
+
         else:
-            ctx.callback.msiWriteRodsLog("ERROR: Response EpicPID not HTTP OK: '{}'".format(response.status_code), 0)
+            ctx.callback.msiWriteRodsLog(
+                "ERROR: Response Help center backend not HTTP OK: '{}'".format(response.status_code), 0
+            )
     except requests.exceptions.RequestException as e:
-        ctx.callback.msiWriteRodsLog("Exception while requesting PID: '{}'".format(e), 0)
+        ctx.callback.msiWriteRodsLog("Exception while requesting Support ticket after process error '{}'".format(e), 0)

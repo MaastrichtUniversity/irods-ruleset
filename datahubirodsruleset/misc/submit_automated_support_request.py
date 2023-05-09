667d15719aa6a51d21c400e5df424ec4bbdec56b
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-05-09 14:11:34 +0200

    Use username in submit_ingest_error_automated_support_request  [DHS-3071, DHS-3053]

--- datahubirodsruleset/misc/submit_automated_support_request.py
+++ datahubirodsruleset/misc/submit_automated_support_request.py
@@ -1,10 +1,16 @@
+from datahubirodsruleset import FALSE_AS_STRING, TRUE_AS_STRING
 from datahubirodsruleset.decorator import make, Output
 
-# /rules/tests/run_test.sh -r submit_ingest_error_automated_support_request -a "email@example.org,P000000001,token-token,LOL" -j
+# /rules/tests/run_test.sh -r submit_ingest_error_automated_support_request -a "jmelius,P000000001,token-token,LOL" -j
+# /rules/tests/run_test.sh -r submit_automated_support_request -a "email@example.org,description,error" -j
 
 
 @make(inputs=[0, 1, 2, 3], outputs=[4], handler=Output.STORE)
-def submit_ingest_error_automated_support_request(ctx, email, project_id, token, error_message):
+def submit_ingest_error_automated_support_request(ctx, username, project_id, token, error_message):
+    import json
+
+    ret = ctx.get_user_attribute_value(username, "email", FALSE_AS_STRING, "result")["arguments"][3]
+    email = json.loads(ret)["value"]
 
     description = (
         "Ingest for dropzone {} (Project {}) has failed, we will contact you when we have more information "
@@ -20,7 +26,7 @@ def submit_automated_support_request(ctx, email, description, error_message):
     from datetime import datetime
 
     # Get the Help Center Backend url
-    help_center_backend_base = ctx.callback.get_env("HC_BACKEND_URL", "true", "")["arguments"][2]
+    help_center_backend_base = ctx.callback.get_env("HC_BACKEND_URL", TRUE_AS_STRING, "")["arguments"][2]
     help_center_request_endpoint = "{}/help_backend/submit_request/automated_process_support".format(
         help_center_backend_base
     )
@@ -48,6 +54,8 @@ def submit_automated_support_request(ctx, email, description, error_message):
                 "ERROR: Response Help center backend not HTTP OK: '{}'".format(response.status_code), 0
             )
     except requests.exceptions.RequestException as e:
-        ctx.callback.msiWriteRodsLog("Exception while requesting Support ticket after process error '{}'".format(e), 0)
+        ctx.callback.msiWriteRodsLog(
+            "ERROR: Exception while requesting Support ticket after process error '{}'".format(e), 0
+        )
 
     return issue_key

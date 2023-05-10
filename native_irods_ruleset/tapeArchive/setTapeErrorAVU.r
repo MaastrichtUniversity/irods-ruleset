48c08058189760894c823ed73daec093eb11e177
jonathanMELIUS <meliusjonathan@gmail.com>
2023-05-10 16:57:58 +0200

    Update setTapeErrorAVU to call submit_tape_error [DHS-3053, DHS-3073, DHS-3074]

--- native_irods_ruleset/tapeArchive/setTapeErrorAVU.r
+++ native_irods_ruleset/tapeArchive/setTapeErrorAVU.r
@@ -1,16 +1,28 @@
-#irule -F setTapeErrorAVU.r "*projectCollection='/nlmumc/projects/P000000001/C000000001'" "*attribute='attr'" "*value='val'" "*message='crash'"
+#irule -F setTapeErrorAVU.r "*projectCollection='/nlmumc/projects/P000000001/C000000001'" "*initiator='jmelius'" "*attribute='attr'" "*value='val'" "*message='crash'"
 
 irule_dummy() {
-    IRULE_setTapeErrorAVU(*projectCollection, *attribute, *value,*message);
+    IRULE_setTapeErrorAVU(*projectCollection, *initiator, *attribute, *value,*message);
 }
 
 
-IRULE_setTapeErrorAVU(*projectCollection, *attribute, *value, *message) {
-     msiAddKeyVal(*metaKV,  *attribute, *value);
-     msiSetKeyValuePairsToObj(*metaKV, *projectCollection, "-C");
-     msiWriteRodsLog("Tape archival/unarchival failed *projectCollection with error status '*value'", 0);
-     failmsg(-1, "*message for *projectCollection");
+IRULE_setTapeErrorAVU(*projectCollection, *initiator, *attribute, *value, *message) {
+    msiAddKeyVal(*metaKV,  *attribute, *value);
+    msiSetKeyValuePairsToObj(*metaKV, *projectCollection, "-C");
+    msiWriteRodsLog("Tape archival/unarchival failed *projectCollection with error status '*value'", 0);
+
+    uuChopPath(*projectCollection, *dir, *collectionID);
+    uuChopPath(*dir, *dir2, *projectID);
+
+    if (*value == "error-archive-failed"){
+        *description = "Archival failed for collection *collectionID in project *projectID"
+        submit_tape_error(*initiator, *description, *message)
+    } else if (*value == "error-unarchive-failed"){
+        *description = "Un-archival failed for collection *collectionID in project *projectID"
+        submit_tape_error(*initiator, *description, *message)
+    }
+
+    failmsg(-1, "*message for *projectCollection");
 }
 
-INPUT *projectCollection='', *attribute='', *value='',*message=''
+INPUT *projectCollection='', *initiator='', *attribute='', *value='',*message=''
 OUTPUT ruleExecOut

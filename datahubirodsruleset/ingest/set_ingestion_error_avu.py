52e7884d4e62c2e6ae32ca6e565c96713976b4cb
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-05-16 11:25:09 +0200

    Add documentation for set_ingestion_error_avu.py[DHS-3071, DHS-3053]

--- datahubirodsruleset/ingest/set_ingestion_error_avu.py
+++ datahubirodsruleset/ingest/set_ingestion_error_avu.py
@@ -1,10 +1,28 @@
-from dhpythonirodsutils.enums import DropzoneState
+# /rules/tests/run_test.sh -r set_ingestion_error_avu -a "/nlmumc/ingest/direct/shiny-curlew,sukkel,P000000001,dlinssen" -u dlinssen
 
+from dhpythonirodsutils.enums import DropzoneState
 from datahubirodsruleset.decorator import make, Output
 
 
 @make(inputs=[0, 1, 2, 3], outputs=[], handler=Output.STORE)
 def set_ingestion_error_avu(ctx, path, message, project_id, username):
+    """
+    Set an ingestion error AVU, create a jira service desk ticket and exit.
+
+    Parameters
+    ----------
+    ctx : Context
+        Combined type of callback and rei struct.
+    path: str
+        path to the dropzone (e.g: /nlmumc/ingest/zones/crazy-frog
+    message: str
+        message to write to rodslog, The reason for the failure. (e.g: "Unable to register PID's for root")
+    project_id: str
+        The project to that the collection that needs to be closed is part of (e.g: P000000010)
+    username: str
+        The user who started the ingestion
+    """
+
     value = DropzoneState.ERROR_INGESTION.value
     ctx.callback.setCollectionAVU(path, "state", value)
     ctx.callback.msiWriteRodsLog("Ingest failed of {} with error status {}".format(path, value), 0)

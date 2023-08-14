caafbef609cb1eeac3b0d56b8a107a370b18bc93
jonathanMELIUS <meliusjonathan@gmail.com>
2023-08-14 14:49:26 +0200

    Bump python_requires version to 3.6 [DHS-3180]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=2.7",
+    python_requires=">=3.6",
     install_requires=[
         "dh-python-irods-utils @ git+https://github.com/MaastrichtUniversity/dh-python-irods-utils.git@v1.1.1#egg=dh-python-irods-utils",
         "cryptography",

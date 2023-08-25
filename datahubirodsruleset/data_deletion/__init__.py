071991c57ec4c3327cefbd758666dd1c16787827
jonathanMELIUS <meliusjonathan@gmail.com>
2023-08-25 12:32:51 +0200

    Add new rule restore_project_collection_user_access [DHS-3166, DHS-3257]

--- datahubirodsruleset/data_deletion/__init__.py
+++ datahubirodsruleset/data_deletion/__init__.py
@@ -1,6 +1,9 @@
 """This sub-package contains the rules related to DataHub data deletion"""
 
 # Public rules
+from datahubirodsruleset.data_deletion.restore_project_collection_user_access import (
+    restore_project_collection_user_access,
+)
 from datahubirodsruleset.data_deletion.revoke_project_collection_user_access import (
     revoke_project_collection_user_access,
 )

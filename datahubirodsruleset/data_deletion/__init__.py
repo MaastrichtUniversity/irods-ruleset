0a1dd0b80814b344f2fc79eeeec8eaefe53070fa
jonathanMELIUS <meliusjonathan@gmail.com>
2023-08-29 13:38:58 +0200

    Add new rule delete_project_data [DHS-3166]

--- datahubirodsruleset/data_deletion/__init__.py
+++ datahubirodsruleset/data_deletion/__init__.py
@@ -2,7 +2,7 @@
 
 # Public rules
 from datahubirodsruleset.data_deletion.delete_project_collection_data import delete_project_collection_data
-from datahubirodsruleset.data_deletion.restore_project_user_access import restore_project_user_access
+from datahubirodsruleset.data_deletion.delete_project_data import delete_project_data
 from datahubirodsruleset.data_deletion.restore_project_collection_user_access import (
     restore_project_collection_user_access,
 )

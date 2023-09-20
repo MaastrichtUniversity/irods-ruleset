f68b6a5fa0e88bfeabb3b10e1b026b0b49120d32
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-09-20 09:30:22 +0200

    Add check for collection pending deletion in delete_project_data.py, Change moment when to set pending deletion in revoke rules[DHS-3166]

--- datahubirodsruleset/data_deletion/revoke_project_user_access.py
+++ datahubirodsruleset/data_deletion/revoke_project_user_access.py
@@ -1,25 +1,44 @@
-# /rules/tests/run_test.sh -r revoke_project_user_access -a "/nlmumc/projects/P000000011"
+# /rules/tests/run_test.sh -r revoke_project_user_access -a "/nlmumc/projects/P000000011,funding_expired,description24"
 import json
 
 from dhpythonirodsutils import formatters
-from dhpythonirodsutils.enums import ProcessAttribute
+from dhpythonirodsutils.enums import DataDeletionAttribute, DataDeletionState
 from genquery import row_iterator, AS_LIST
 
 from datahubirodsruleset import IRODS_BACKUP_ACL_BASE_PATH, IRODS_ZONE_BASE_PATH
 from datahubirodsruleset.data_deletion.restore_project_user_access import map_access_name_to_access_level
+from datahubirodsruleset.data_deletion.revoke_project_collection_user_access import SERVICE_ACCOUNT_ALLOW_LIST
 from datahubirodsruleset.decorator import make, Output
 
-# TODO - POC rule
-#   Refactor this rule during DHS-3024
 
+@make(inputs=[0, 1, 2], outputs=[], handler=Output.STORE)
+def revoke_project_user_access(ctx, user_project, reason, description):
+    """
+    After a user request a project deletion, all users accesses need to be revoked immediately including
+    service accounts not on the allow list. Including all collections that have not yet been deleted.
+    Additionally:
+     * Check if there are any ongoing process linked to the project, this includes dropzones and pending deletions.
+      If true, stop the rule execution
+     * The 'deletion metadata' need to be set as AVUs on the project
+     * Backup of ACL is created
 
-@make(inputs=[0], outputs=[], handler=Output.STORE)
-def revoke_project_user_access(ctx, user_project):
+    Parameters
+    ----------
+    ctx : Context
+        Combined type of callback and rei struct.
+    user_project : str
+         The absolute path of the project
+    reason : str
+        The reason of the deletion
+    description : str
+        An optional description providing additional details, empty string if not provided by the form/user
+    """
     project_id = formatters.get_project_id_from_project_path(user_project)
-    has_active_drop_zones = check_active_dropzone_by_project_id(ctx, project_id)
-    has_active_processes = check_active_processes_by_project_id(ctx, project_id)
 
-    if has_active_drop_zones or has_active_processes:
+    output = ctx.callback.get_project_process_activity(project_id, "result")["arguments"][1]
+    process_activity = json.loads(output)
+
+    if process_activity["has_process_activity"]:
         ctx.callback.msiExit("-1", "Stop execution, active proces(ses) found for project {}".format(user_project))
         return
 
@@ -27,10 +46,11 @@ def revoke_project_user_access(ctx, user_project):
     try:
         ctx.callback.msiCollCreate(backup_project, 1, 0)
     except RuntimeError:
-        print("ERROR: msiCollCreate")
+        ctx.callback.msiWriteRodsLog("ERROR: Could not create backup project '{}'".format(backup_project), 0)
+        ctx.callback.msiExit("-1", "Stop execution, could not create backup project '{}'".format(backup_project))
+        return
 
     ctx.callback.msiWriteRodsLog("INFO: Create ACL backup for project '{}'".format(backup_project), 0)
-    apply_batch_collection_avu_operation(ctx, user_project, "add")
 
     for result in row_iterator(
         "COLL_ACCESS_USER_ID, COLL_ACCESS_NAME, COLL_ACCESS_TYPE",
@@ -47,73 +67,22 @@ def revoke_project_user_access(ctx, user_project):
             account_type = account[1]
             ctx.callback.msiSetACL("default", user_access, account_name, backup_project)
 
-            if account_type != "rodsadmin" and "service-" not in account_name:
+            if account_type != "rodsadmin" and account_name not in SERVICE_ACCOUNT_ALLOW_LIST:
                 ctx.callback.msiSetACL("default", "null", account_name, user_project)
 
     ctx.callback.msiWriteRodsLog("INFO: Users ACL revoked for project '{}'".format(user_project), 0)
 
     ctx.callback.msiWriteRodsLog("INFO: Revoke users ACL for collections in project '{}'".format(user_project), 0)
     for proj_coll in row_iterator("COLL_NAME", "COLL_PARENT_NAME = '{}'".format(user_project), AS_LIST, ctx.callback):
-        ctx.revoke_project_collection_user_access(proj_coll[0], "deletionReason", "deletionReasonDescription")
+        project_collection_path = proj_coll[0]
+        # For already deleted collections we do not revoke anything, this is already done
+        output = ctx.callback.get_collection_attribute_value(
+            project_collection_path, DataDeletionAttribute.STATE.value, "result"
+        )["arguments"][2]
+        data_deletion_status = json.loads(output)["value"]
+        if data_deletion_status == DataDeletionState.DELETED.value:
+            continue
 
+        ctx.revoke_project_collection_user_access(project_collection_path, reason, description)
 
-def check_active_dropzone_by_project_id(ctx, project_id):
-    parameters = "COLL_NAME, META_COLL_ATTR_VALUE"
-    conditions = (
-        "COLL_PARENT_NAME in ('/nlmumc/ingest/zones','/nlmumc/ingest/direct') "
-        "AND META_COLL_ATTR_NAME = 'project' "
-        "AND META_COLL_ATTR_VALUE = '{}'".format(project_id)
-    )
-    for result in row_iterator(parameters, conditions, AS_LIST, ctx.callback):
-        token = result[0]
-        dropzone_project_id = result[1]
-        print("{} -> {}".format(token, dropzone_project_id))
-        return True
-
-    return False
-
-
-def check_active_processes_by_project_id(ctx, project_id):
-    parameters = "COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE"
-    conditions = "META_COLL_ATTR_NAME in ('{}', '{}', '{}') AND COLL_PARENT_NAME LIKE '/nlmumc/projects/{}' ".format(
-        ProcessAttribute.ARCHIVE.value,
-        ProcessAttribute.UNARCHIVE.value,
-        ProcessAttribute.EXPORTER.value,
-        project_id,
-    )
-
-    for result in row_iterator(parameters, conditions, AS_LIST, ctx.callback):
-        name = result[0]
-        process = result[1]
-        print("{} -> {}".format(name, process))
-        return True
-
-    return False
-
-
-def apply_batch_collection_avu_operation(ctx, collection, operation):
-    json_input = {
-        "entity_name": collection,
-        "entity_type": "collection",
-        "operations": [
-            {
-                "operation": operation,
-                "attribute": "deletionReason",
-                "value": "deletionReason",
-            },
-            {
-                "operation": operation,
-                "attribute": "deletionReasonDescription",
-                "value": "deletionReasonDescription",
-            },
-            {
-                "operation": operation,
-                "attribute": "deletionState",
-                "value": "pending-for-deletion",
-            },
-        ],
-    }
-    str_json_input = json.dumps(json_input)
-    ctx.msi_atomic_apply_metadata_operations(str_json_input, "")
-    message = "INFO: {} deletion metadata for {}".format(operation, collection)
-    ctx.callback.msiWriteRodsLog(message, 0)
+    ctx.callback.setCollectionAVU(user_project, DataDeletionAttribute.STATE.value, DataDeletionState.PENDING.value)

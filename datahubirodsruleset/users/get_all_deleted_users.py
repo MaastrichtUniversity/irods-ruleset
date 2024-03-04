8f6b55329edd890dc97a6347e53ae431cded31b9
Ricardo Juhasz <ricardo.juhasz@maastrichtuniversity.nl>
2024-03-04 10:01:32 +0100

    [draft] Inactivate user script rule [DHDO-1194]

--- datahubirodsruleset/users/get_all_deleted_users.py
+++ datahubirodsruleset/users/get_all_deleted_users.py
@@ -1,6 +1,5 @@
 # /rules/tests/run_test.sh -r get_all_deleted_users -j
 from genquery import row_iterator, AS_LIST  # pylint: disable=import-error
-
 from datahubirodsruleset.decorator import make, Output
 
 
@@ -13,3 +12,24 @@ def get_all_deleted_users(ctx):
         output.append(account[0])
 
     return output
+
+    #username = {output.spit(",")}
+    #attribute_to_query = "'dataSteward', 'OBI:0000103'"
+    #rule_result = {}
+
+
+    #for query_result in row_iterator(
+    #    "COLL_NAME, META_COLL_ATTR_NAME, META_COLL_ATTR_VALUE",
+    #    "META_COLL_ATTR_VALUE in ({}) AND META_COLL_ATTR_NAME in ({})".format(username, attribute_to_query),
+    #    AS_LIST,
+    #    ctx.callback,
+    #):
+    #    username = query_result[0]
+    #    attribute = query_result[1]
+    #    value = query_result[2]
+    #    if username not in rule_result:
+    #        rule_result[username] = {attribute: value}
+    #    else:
+    #        rule_result[username][attribute] = value
+
+    #return rule_result

8fbc2aa4681144d8bd2107be0fa096bebfeb2e83
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2024-11-06 14:34:54 +0100

    Fix tape issues in acceptance [DHDO-1500]

--- native_irods_ruleset/tapeArchive/dmattr.r
+++ native_irods_ruleset/tapeArchive/dmattr.r
@@ -1,6 +1,12 @@
 
 dmattr(*data, *svr, *Out){
     # This part is always called remotely on the tape server
-    msiExecCmd("dmattr", *data, *svr, "", "", *dmRes);
+    # This is quoted because dmattr does not support spaces
+    msiExecCmd("dmattr", "'*data'", *svr, "", "", *dmRes);
     msiGetStdoutInExecCmdOut(*dmRes,*Out);
+    msiGetStderrInExecCmdOut(*dmRes,*Err);
+    if ( strlen(*Err) > 0 ) {
+        msiWriteRodsLog("Error occured during dmattr", 0)
+        msiWriteRodsLog(*Err, 0)
+    }
 }

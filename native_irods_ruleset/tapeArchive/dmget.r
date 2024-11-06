4f299b4e46caaa40c0951eaab5e372da3af4726f
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2024-11-06 15:23:19 +0100

    Also add quotes to 'dmget' [DHDO-1500]

--- native_irods_ruleset/tapeArchive/dmget.r
+++ native_irods_ruleset/tapeArchive/dmget.r
@@ -1,6 +1,11 @@
 
 dmget(*data, *svr){
-    msiExecCmd("dmget", "*data", "*svr", "", "", *dmRes);
+    msiExecCmd("dmget", "'*data'", "*svr", "", "", *dmRes);
     msiGetStdoutInExecCmdOut(*dmRes,*dmStat);
     msiWriteRodsLog("DEBUG: $userNameClient:$clientAddr- Archive dmget started on *svr:*data. Returned Status- *dmStat.", 0);
+    msiGetStderrInExecCmdOut(*dmRes,*Err);
+    if ( strlen(*Err) > 0 ) {
+        msiWriteRodsLog("Error occured during dmattr", 0)
+        msiWriteRodsLog(*Err, 0)
+    }
 }

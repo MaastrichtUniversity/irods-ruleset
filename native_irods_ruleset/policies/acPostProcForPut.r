889dd75bdd8d6c0e78835d64f716d59f47b9ffd7
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2025-06-02 09:50:12 +0200

    Modify acPostProcForPut again due to bug [DHDO-1731]

--- native_irods_ruleset/policies/acPostProcForPut.r
+++ native_irods_ruleset/policies/acPostProcForPut.r
@@ -11,10 +11,11 @@ acPostProcForPut {
          # Get the creator AVU from the collection, if it exists, that means the collection already is fully ingested
          getCollectionAVU("/nlmumc/projects/*project/*collection","creator",*creator,"","false");
          if(*creator == ""){
-              # This policy will fire twice for every file (replicated resources) or once for passthru resources (um-hnas)
-              # For mounted ingest, it will create replNum 0 and 1 (replicated) or 0 (passthru), because these are the first copies of the data
-              # Both will fire with replNum = 0, so that is why we choose that one here
-              if(str($replNum) == "0") {
+            ##   This policy will fire twice for every file (replicated resources) or once for passthru resources (um-hnas)
+            ##   For mounted ingest, it will create replNum 0 and 1 (replicated) or 0 (passthru), because these are the first copies of the data
+            ##   Both will fire with replNum = 0, so that is why we choose that one here
+            # The above is irrelevant for now due to a bug found whilst working on DHDO-1731. For more info, see test_pep_api_data_obj_put_post
+              if(str($replNum) == "1") {
                    getCollectionAVU("/nlmumc/projects/*project/*collection","sizeIngested",*sizeIngested,"","false");
                    *sizeIngested = *sizeIngested + double($dataSize);
                    msiAddKeyVal(*metaKV,  'sizeIngested', str(*sizeIngested));

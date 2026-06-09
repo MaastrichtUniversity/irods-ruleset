5dd3ed46ad4afe0e580ea503179daefa07717dea
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2026-06-09 16:39:08 +0200

    Add dropzone validation for stale or locked files and test cases [DHDO-2131]

--- datahubirodsruleset/validation/validate_dropzone.py
+++ datahubirodsruleset/validation/validate_dropzone.py
@@ -3,6 +3,7 @@ import irods_types  # pylint: disable=import-error
 
 from dhpythonirodsutils import formatters
 from dhpythonirodsutils.enums import ProjectAVUs, DropzoneState
+from genquery import row_iterator, AS_LIST  # pylint: disable=import-error
 
 from datahubirodsruleset.decorator import make, Output
 from datahubirodsruleset.formatters import format_project_path
@@ -43,6 +44,14 @@ def validate_dropzone(ctx, dropzone_path, username, dropzone_type):
 
     # Check if dropzone exists
     validation_errors = check_if_dropzone_exists(ctx, dropzone_path, validation_errors)
+    # If validation errors at this point, return immediately since the rest of the validation relies on the dropzone existing
+    if validation_errors:
+        return {"project_id": "dz-does-not-exist", "validation_errors": validation_errors}
+    
+    ctx.callback.msiWriteRodsLog(f"Starting validation of {dropzone_path}:", 0)
+
+    # Set 'state' AVU to 'validating'
+    ctx.callback.setCollectionAVU(dropzone_path, "state", DropzoneState.VALIDATING.value)
 
     # Get dropzone metadata
     project_id = ctx.callback.getCollectionAVU(dropzone_path, "project", "", "", TRUE_AS_STRING)["arguments"][2]
@@ -50,6 +59,9 @@ def validate_dropzone(ctx, dropzone_path, username, dropzone_type):
 
     # Check if project path exists
     validation_errors = check_if_project_exists(ctx, project_path, project_id, validation_errors)
+    # If validation errors at this point, return immediately since the rest of the validation relies on the project existing
+    if validation_errors:
+        return {"project_id": project_id, "validation_errors": validation_errors}
 
     # Check if user is allowed to start this specific ingest
     validation_errors = check_if_user_is_allowed_to_start_ingest(
@@ -65,11 +77,6 @@ def validate_dropzone(ctx, dropzone_path, username, dropzone_type):
     if not formatters.format_string_to_boolean(available):
         validation_errors.append(f"The project or ingest resource is disabled for this project '{project_id}'")
 
-    ctx.callback.msiWriteRodsLog(f"Starting validation of {dropzone_path}:", 0)
-
-    # Set 'state' AVU to 'validating'
-    ctx.callback.setCollectionAVU(dropzone_path, "state", DropzoneState.VALIDATING.value)
-
     # Start metadata validation
     validation_result = ctx.callback.validate_metadata(dropzone_path, "")["arguments"][1] == TRUE_AS_STRING
     if not validation_result:
@@ -83,6 +90,8 @@ def validate_dropzone(ctx, dropzone_path, username, dropzone_type):
     # Check if the dropzone is valid for ingestion
     # see bug https://github.com/irods/irods/issues/7302
     validation_errors = is_dropzone_ingestable(ctx, dropzone_path, validation_errors)
+    if dropzone_type == "direct":
+        validation_errors = does_dropzone_contain_stale_or_locked_files(ctx, dropzone_path, validation_errors)
 
     return {"project_id": project_id, "validation_errors": validation_errors}
 
@@ -140,4 +149,20 @@ def check_if_user_is_allowed_to_start_ingest(
         creator = ctx.callback.getCollectionAVU(dropzone_path, "creator", "", "", TRUE_AS_STRING)["arguments"][2]
         if creator != username:
             validation_errors.append(f"User '{username}' is not the creator of dropzone '{dropzone_path}'")
+    return validation_errors
+
+def does_dropzone_contain_stale_or_locked_files(ctx, dropzone_path, validation_errors):
+    for failed_data_object in row_iterator(
+        "COLL_NAME,DATA_NAME,DATA_REPL_NUM,DATA_REPL_STATUS",
+        f"COLL_NAME LIKE '{dropzone_path}%' AND DATA_REPL_STATUS != '1'",
+        AS_LIST,
+        ctx.callback
+    ):
+        data_path = f"{failed_data_object[0]}/{failed_data_object[1]}"
+        if failed_data_object[3] == "0":
+            validation_errors.append(f"Dropzone contains stale file '{data_path}'")
+        elif failed_data_object[3] == "2":
+            validation_errors.append(f"Dropzone contains locked file '{data_path}'")
+        else:
+            validation_errors.append(f"Dropzone contains file with unknown replication status '{data_path}'")
     return validation_errors
